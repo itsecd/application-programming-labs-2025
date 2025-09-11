b202b4dbe9da37eca5e078caf100afa1ed223006
BlaZeriTUwU <schumakownikita@gmail.com>
2025-09-11 08:40:42 +0400

    Исправлены замечания: разделены функции, добавлена кодировка

--- laba/main.py
+++ laba/main.py
@@ -1,9 +1,10 @@
 import re
 import argparse
 from typing import List, Tuple
+import sys
 
 KEY_PATTERN = re.compile(
-    r"Фамилия:\s*(?P<firstname>.+?)\nИмя:\s*(?P<lastname>.+?)\n"
+    r"Фамилия:\s*(?P<lastname>.+?)\nИмя:\s*(?P<firstname>.+?)\n"
 )
 OLD_NUMBER_PATTERN = re.compile(r"^\d+\)")
 
@@ -12,24 +13,25 @@ def parser_t():
     функция нужна для ввода названий файлов
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('first_file', type=str, help='Путь к исходному файлу')
-    parser.add_argument('-lf', '--last_file', default='output.txt', type=str, help='Путь к файлу для сохранения результата')
+    parser.add_argument('source_file', type=str, help='Путь к исходному файлу')
+    parser.add_argument('-of', '--output_file', default='output.txt', type=str, help='Путь к файлу для сохранения результата')
 
     args = parser.parse_args()
     return args
 
-
-def read_and_split_into_blocks(filepath: str) -> List[str]:
+def open_file(filepath):
     """
-    читает файл и разделяет его на текстовые блоки по записям.
+    функция для прочтения файла
     """
-    try:
-        with open(filepath, "r") as file:
-            full_text = file.read()
-    except FileNotFoundError:
-        print(f"Ошибка: Файл '{filepath}' не найден.")
-        return []
+    with open(filepath, "r", encoding="utf-8") as file:
+        full_text = file.read()
+    return full_text
 
+
+def split_into_blocks(full_text: str) -> List[str]:
+    """
+    разделяет текст на текстовые блоки по записям.
+    """
     raw_blocks = re.split(r'(\d+\)\n)', full_text)[1:]
     grouped_blocks = [
         raw_blocks[i] + raw_blocks[i+1]
@@ -59,7 +61,7 @@ def write_sorted_blocks_to_file(
     """
     Записывает отсортированные блоки в файл с новой нумерацией.
     """
-    with open(filepath, "w") as output_file:
+    with open(filepath, "w", encoding="utf-8") as output_file:
         for index, s_tuple in enumerate(sorted_list, 1):
             new_number_str = f"{index})"
             new_block = OLD_NUMBER_PATTERN.sub(new_number_str, s_tuple[2])
@@ -73,20 +75,22 @@ def main() -> None:
 
     a = parser_t()
 
-    input_filename = a.first_file
-    output_filename = a.last_file
+    input_filename = a.source_file
+    output_filename = a.output_file
 
     print(f"Чтение файла '{input_filename}'...")
-    all_blocks = read_and_split_into_blocks(input_filename)
-    if not all_blocks:
-        print("Файл пуст или не найден. Завершение работы.")
-        return
+    try:
+        full_text = open_file(input_filename)
+        blocks = split_into_blocks(full_text)
 
-    sortable_data = create_sortable_list(all_blocks)
-    final_sorted_list = sorted(sortable_data, key=lambda item: (item[0], item[1]))
-    write_sorted_blocks_to_file(output_filename, final_sorted_list)
+        sortable_data = create_sortable_list(blocks)
+        final_sorted_list = sorted(sortable_data, key=lambda item: (item[1], item[0]))
+        write_sorted_blocks_to_file(output_filename, final_sorted_list)
 
-    print("\nПроцесс успешно завершен!")
+        print("\nПроцесс успешно завершен!")
+    except FileNotFoundError:
+        print("\nПроцесс завершился с ошибкой")
+        sys.exit()
 
 if __name__ == '__main__':
     main()
