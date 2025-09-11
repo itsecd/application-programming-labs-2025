e1748478ee0302ebc4b3aceb465d8dde329cad79
Povelitel-kiber-ezikov <mr.timur1705@gmail.com>
2025-09-11 22:46:42 +0400

    Добавил функцию выделения фамилии и даты рождения, а также сделал функцию, которая приводит даты к единому формату

--- lab1_var24.py
+++ lab1_var24.py
@@ -1,10 +1,38 @@
 import re
 import argparse
+from typing import List, Tuple
 
 def read_file(filename: str) -> str:
     """
-    читаю содержимое файла
+    чтение одержимого файла
     """
-    with open(filename,'r', encoding="utf-8") as filename:
-        data = filename.read()
+    with open(filename,'r', encoding="utf-8") as file:
+        return file.read()
+
+
+def extracting_last_name_date_of_birth(data:str) -> List[Tuple[str, str]]:
+    """
+    выделяю из всех данных только
+    Фамилию и дату рождения
+    """
+    pattern = r'Фамилия:\s*([^\n]+)\s*(?:.*?\n)*?Дата рождения:\s*(\d{1,2}[./-]\d{1,2}[./-]\d{4})'
+    return re.findall(pattern, data, re.IGNORECASE)
+
+def normolize_date(date_of_birth: str) -> str:
+    """
+    привожу даты к единому формату DD.MM.YYYY
+    """
+    date = date_of_birth.strip() # удаляю пробелы по бокам
+
+    normal_date = re.sub(r'[-/]', '.', date)
+
+    return normal_date
+
+
+
+
+
+
+
+
 
