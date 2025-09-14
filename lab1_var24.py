e251414b0f74530a2e6863b775af1e7cc0493d45
Povelitel-kiber-ezikov <mr.timur1705@gmail.com>
2025-09-14 14:08:35 +0400

    Добавил сортировку по дате рождения, слегка поменял функции проверки и нормализации, а также удалил функцию вычисления возраста, из-за ненадобности

--- lab1_var24.py
+++ lab1_var24.py
@@ -12,33 +12,40 @@ def read_file(filename: str) -> str:
 
 def extracting_last_name_date_of_birth(data:str) -> List[Tuple[str, str]]:
     """
-    выделяю из всех данных только
-    Фамилию и дату рождения
+    выделение из всех данных только
+    Фамилии и дат рождения
     """
     pattern = r'Фамилия:\s*([^\n]+)\s*(?:.*?\n)*?Дата рождения:\s*(\d{1,2}[./-]\d{1,2}[./-]\d{4})'
     return re.findall(pattern, data, re.IGNORECASE)
 
-def normolize_date(date_of_birth: str) -> str:
+def normalize_date(date_of_birth: str) -> str:
     """
-    привожу даты к единому формату DD.MM.YYYY
+    приведение даты к единому формату YYYY-MM-DD
     """
     date = date_of_birth.strip() # удаляю пробелы по бокам
 
-    normal_date = re.sub(r'[-/]', '.', date)
+    normal_date = re.sub(r'[/.]', '-', date)
+    day, month, year = normal_date.split('-')
 
-    return normal_date
+    day = day.zfill(2)
+    month = month.zfill(2)
 
-def check_date(date_of_birth: str) -> bool:
+    return f"{year}-{month}-{day}"
+
+def check_date(date: str) -> bool:
     """
     проверка даты
     """
-    date = normolize_date(date_of_birth)
 
-    penis = re.split(r'[.]', date) # ПОМЕНЯЙ ЕБЛАН
+    penis = re.split(r'[-]', date) # ПОМЕНЯЙ ЕБЛАН
 
-    day = int(penis[0])
+    if len(penis[2]) != 2 or len(penis[1]) !=2 or len(penis[0]) != 4:
+        return False
+
+
+    day = int(penis[2])
     month = int(penis[1])
-    year = int(penis[2])
+    year = int(penis[0])
 
     #проверка на адекватность даты
     if not(1<= day <= 31) or not(1 <= month <= 12) or not(1900 <= year < 2026):
@@ -52,27 +59,33 @@ def check_date(date_of_birth: str) -> bool:
 
     return True
 
-def defining_the_age(date_of_birth: str) -> int:
+def sort_date(data: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
     """
-    вычисление возраста
+    сортировка по дате рождения
     """
-    date = normolize_date(date_of_birth)
-    now_year = localtime(date).tm_year
-    now_day = localtime(date).tm_yday
-    now_month = localtime(date).tm_mon
-    penis = re.split(r'[.]', date)  # ПОМЕНЯЙ ЕБЛАН
-    year = int(penis[2])
-    day = int(penis[0])
-    month = int(penis[1])
 
-    if(month < now_month):
-        if(day< now_day):
-            return now_year - year
-        else:
-            return now_year - year -1
+    sorted_last_name_and_date_of_birth : List[Tuple[str, str]] = []
+    for last_name, date_of_birth in data:
+        norm_date = normalize_date(date_of_birth)
+        if check_date(norm_date):
+           sorted_last_name_and_date_of_birth.append((last_name, norm_date))
 
-    else:
-        return now_year - year - 1
+    sorted_last_name_and_date_of_birth.sort(key=lambda x: x[1])
+
+
+
+
+    return sorted_last_name_and_date_of_birth
+
+a = read_file("data.txt")
+b = extracting_last_name_date_of_birth(a)
+c = sort_date(b)
+print(c)
+
+
+
+
+
 
 
 
