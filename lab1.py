38f5e404fe232acd26dde213479d2a7e867a17a6
shydoke <ban33665@gmail.com>
2025-09-15 19:27:18 +0400

    готовый функционал

---
+++ lab1.py
@@ -0,0 +1,16 @@
+﻿import re
+from collections import Counter
+
+with open('data.txt', 'r', encoding='utf-8') as file:
+    content = file.read()
+
+codes = re.findall(r'[+78][\s\(]*(\d{3})', content)
+
+if codes:
+    code_counter = Counter(codes)
+    code, count = code_counter.most_common(1)[0]
+    print(f"Самый частый код оператора: {code}")
+    print(f"Повторений: {count}")
+
+else:
+    print("Коды операторов не найдены")
