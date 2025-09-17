e73322cea7f4b7df2ebaad890e1caae733140aba
Pavel-Gornov <gornovpavel30@yandex.ru>
2025-09-17 23:21:57 +0400

    Выполнение задачи и вывод в файл

--- main.py
+++ main.py
@@ -5,15 +5,26 @@ def main(filename: str) -> None:
     pattern = r"@\w+.\w+"
 
     with open(f"{filename}", mode="r", encoding="utf8") as inp:
-        domains = re.findall(pattern, inp.read())
-    print(domains)
+        domains = list(map(lambda x: x[1::], re.findall(pattern, inp.read()))) 
+        # чтобы избавиться от лишней '@' в начале ^
+    
+    results = dict.fromkeys(domains, 0)
+    for domain in domains:
+        results[domain] += 1
+
+    with open("output.txt", mode="w", encoding="utf8") as out:
+        for dom, num in sorted(results.items(), key=lambda x: x[1], reverse=True):
+            out.write(f"{dom}: {num}\n")
 
 
 if __name__ == "__main__":
     import argparse
-
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", type=str, help="путь до файла с данными")
     args = parser.parse_args()
 
-    main(args.file)
+    try:
+        main(args.file)
+    except Exception as e:
+        print(e)
