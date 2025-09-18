1e23fe7f4bbcebf635ed28afb3d1136d9fe27e2d
Povelitel-kiber-ezikov <mr.timur1705@gmail.com>
2025-09-18 10:03:00 +0400

    Изменение валидации даты рождения на datetime

--- lab1_var24.py
+++ lab1_var24.py
@@ -1,5 +1,6 @@
-import re
 import argparse
+import re
+from datetime import datetime, date
 from typing import List, Tuple
 
 
@@ -12,110 +13,98 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument("-o", "--output_file", type=str, required=True, help="Имя выходного файла")
     return parser.parse_args()
 
+
 def read_file(filename: str) -> str:
     """
     чтение одержимого файла
     """
-    with open(filename,'r', encoding="utf-8") as file:
-        return file.read()
+    try:
+        with open(filename, 'r', encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Файл не был найден")
 
-def extracting_last_name_date_of_birth(data:str) -> List[Tuple[str, str]]:
+
+def extracting_last_name_date_of_birth(data: str) -> List[Tuple[str, str]]:
     """
     выделение из всех данных только
     Фамилии и дат рождения
     """
     pattern = r'Фамилия:\s*([^\n0-9]+)\s*(?:.*?\n)*?Дата рождения:\s*(\d{1,2}[./-]\d{1,2}[./-]\d{4})'
-    return re.findall(pattern, data, re.IGNORECASE)
+    return re.findall(pattern, data)
+
 
 def normalize_date(date_of_birth: str) -> str:
     """
-    приведение даты к единому формату YYYY-MM-DD
+    приведение даты к единому формату DD-MM-YYYY
     """
-    try:
-        date = date_of_birth.strip() # удаляю пробелы по бокам
 
-        normal_date = re.sub(r'[/.]', '-', date)
-        day, month, year = normal_date.split('-')
+    date = date_of_birth.strip()  # удаляю пробелы по бокам
 
-        day = day.zfill(2)
-        month = month.zfill(2)
+    normalized_date = re.sub(r'[/.]', '-', date)
+    day, month, year = normalized_date.split('-')
 
-        return f"{year}-{month}-{day}"
-    except ValueError:
-        print(f"Неверный формат даты {date_of_birth}")
+    day = day.zfill(2)
+    month = month.zfill(2)
 
-def check_date(date: str) -> bool:
+    return f"{day}-{month}-{year}"
+
+
+def is_date_correct(date: str) -> bool:
     """
     проверка даты
     """
     try:
         part_date = re.split(r'[-]', date)
 
-        if len(part_date[2]) != 2 or len(part_date[1]) !=2 or len(part_date[0]) != 4:
-            return False
-
-
-        day = int(part_date[2])
+        day = int(part_date[0])
         month = int(part_date[1])
-        year = int(part_date[0])
+        year = int(part_date[2])
 
-        # проверка на адекватность даты
-        if not(1<= day <= 31) or not(1 <= month <= 12) or not(1900 <= year < 2026):
-            return False
-        # проверка на февраль
-        if month == 2 and day > 29:
-            return False
-        # проверка на адекватность даты в месяцах с 30 днями
-        if month in [4,6,9,11] and day > 30:
-            return False
+        year_now = datetime.today().year
 
+        datetime(year, month, day)
+
+        if not (1900 <= year <= year_now):
+            return False
         return True
-    except(ValueError, TypeError):
+    except TypeError:
+        return False
+    except ValueError:
         return False
 
-def sort_date(data: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
+
+def sort_date(data: List[Tuple[str, str]]) -> List[Tuple[str, date]]:
     """
     сортировка по дате рождения
     """
 
-    sorted_last_name_and_date_of_birth : List[Tuple[str, str]] = []
-    unformat_date: List[Tuple[str, str]] = []
+    sorted_last_name_and_date_of_birth: List[Tuple[str, date]] = []
 
     for last_name, date_of_birth in data:
-        norm_date = normalize_date(date_of_birth)
-        if check_date(norm_date):
-           unformat_date.append((last_name, norm_date))
+        normalized_date = normalize_date(date_of_birth)
+        if is_date_correct(normalized_date):
+            normalized_date = datetime.strptime(normalized_date, "%d-%m-%Y").date()
+            sorted_last_name_and_date_of_birth.append((last_name, normalized_date))
 
-    unformat_date.sort(key=lambda x: x[1])
-
-    for last_name, date_of_birth in unformat_date:
-        norm_format_date = return_format_date(date_of_birth)
-        sorted_last_name_and_date_of_birth.append((last_name, norm_format_date))
+    sorted_last_name_and_date_of_birth.sort(key=lambda x: x[1])
 
     return sorted_last_name_and_date_of_birth
 
-def return_format_date(YYYY_format: str) -> str:
-    """
-    возврат даты в подходящий формат DD-MM-YYYY
-    """
-    year, month, day = YYYY_format.split('-')
-    return f"{day}-{month}-{year}"
 
-def format_in_output(data: List[Tuple[str, str]]) -> List[str]:
+def format_in_output(data: List[Tuple[str, date]]) -> List[str]:
     """
     Перевод в конечный формат List
     """
 
-    data_output = []
+    return [f"{last_name}: {date_of_birth}" for last_name, date_of_birth in data]
 
-    for last_name, date_of_birth in data:
-        data_output.append(f"{last_name}: {date_of_birth}")
-    return data_output
 
 def write_to_file(filename: str, all_data: List[str]) -> None:
     with open(filename, 'w', encoding="utf-8") as file:
         for data in all_data:
             file.write(data + '\n')
+
 
 def main() -> None:
     """
@@ -134,18 +123,9 @@ def main() -> None:
         list_sort_date = format_in_output(sort_last_name_date)
 
         write_to_file(args.output_file, list_sort_date)
-
-    except FileNotFoundError:
-        print("ОШИБКА: файл не найден")
-
     except Exception as e:
         print(f"Ошибка при выполнении программы: {e}")
 
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
