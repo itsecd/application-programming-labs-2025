5df929e4602b5b9831e88c194c2146b1e586d20e
Ivan Barabanov <barabanov_vanechka06@mail.ru>
2025-09-20 20:18:44 +0300

    Добавил поиск нужных анкет

--- main.py
+++ main.py
@@ -23,16 +23,14 @@ def find_goal_anketes(anketes:list[str])->list[str]:
     """
     Поиск женских анкет с именами на букву А
     """
-    result=list()
+    result = list()
     for ank in anketes:
-        if re.fullmatch(r"Имя:\s\b(А\w*) Пол:\s([Жж])",ank):
-            result+=[ank]
-            print(ank)
-            input()
-    return ank
+        if re.search(r"Имя:\sА\w*\nПол:\s[Жж]", ank) is not None:
+            result += [ank]
+    return result
+
 
 data = read_file()
 data_list = differentiate_by_anketes(data)
-
 res = find_goal_anketes(data_list)
-print(res)
+print(len(res))
