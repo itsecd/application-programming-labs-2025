7aaa40281cc528858b64090caa4672bc7318813c
clayw <clayworks11337@gmail.com>
2025-09-21 11:29:32 +0400

    Pattern and counter update

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,16 +2,21 @@
 #Выведите их количество на экран и сохраните найденные анкеты в новый файл.
 import re
 
-def count_correct_numbers(text):
-	pattern = r"(8|\+7)+[ (]*\d{3}[ )]*\d{3}[ -]*\d{2}[ -]*\d{2}"
-	count = len(re.findall(pattern, text))
-	return count
+def correct_number(text):
+	start_string = r"[а-яА-Я\sa-z]+: "
+	pattern1 = start_string + r"(\+7|8)\d{10}"
+	pattern2 = start_string + r"(\+7|8)( \(\d{3}\) )\d{3}[ -]{1}\d{2}[ -]\d{2}"
+	pattern3 = start_string + r"(\+7|8)( \d{3} )\d{3}[ -]{1}\d{2}[ -]\d{2}"
+	return re.match(pattern1, text) or re.match(pattern2, text) or re.match(pattern3, text)
 
 
+count_correct_number = 0
 
-
-with open("data.txt", "r") as file:
+with open("data.txt", "r", encoding="utf-8") as file:
 	text = file.read()
-print(text)
+text = text.splitlines()
 
-print(count_correct_numbers(text))
+for line in text:
+	if (correct_number(line)):
+		count_correct_number+=1
+print(count_correct_number)
