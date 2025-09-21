f4bd3c8afc382a5748aa80698fb1fb527b6a6c1d
clayw <clayworks11337@gmail.com>
2025-09-21 11:00:09 +0400

    Regular expressions added

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,1 +1,17 @@
+#Найдите анкеты людей, у которых указан номер телефона (в корректном формате).
+#Выведите их количество на экран и сохраните найденные анкеты в новый файл.
+import re
 
+def count_correct_numbers(text):
+	pattern = r"(8|\+7)+[ (]*\d{3}[ )]*\d{3}[ -]*\d{2}[ -]*\d{2}"
+	count = len(re.findall(pattern, text))
+	return count
+
+
+
+
+with open("data.txt", "r") as file:
+	text = file.read()
+print(text)
+
+print(count_correct_numbers(text))
