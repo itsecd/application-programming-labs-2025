1768027f651605dcc868ab7ee3376af96b8654aa
clayw <clayworks11337@gmail.com>
2025-09-22 23:05:37 +0400

    Minor update

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-import re
 import argparse
+import re
 
 
-def correct_number(text):
+def correct_number(text) -> re.Match:
     """
     Проверка совпадения номера по заданному шаблону.
     """
-    start_string = r"[а-яА-Я\sa-zA-Z]+: "
-    pattern1 = start_string + r"(\+7|8)\d{10}"
-    pattern2 = start_string + r"(\+7|8)( \(\d{3}\) )\d{3}[ -]{1}\d{2}[ -]\d{2}"
-    pattern3 = start_string + r"(\+7|8)( \d{3} )\d{3}[ -]{1}\d{2}[ -]\d{2}"
-    return re.match(
-        pattern1,
-        text) or re.match(
-        pattern2,
-        text) or re.match(
-            pattern3,
-        text)
+
+    pattern1 = r"^Номер телефона или email: (\+7|8)\d{10}$"
+    pattern2 = r"^Номер телефона или email: (\+7|8)( \(\d{3}\) )\d{3}[ -]{1}\d{2}[ -]\d{2}$"
+    pattern3 = r"^Номер телефона или email: (\+7|8)( \d{3} )\d{3}[ -]{1}\d{2}[ -]\d{2}$"
+    return re.match(pattern1, text) or re.match(pattern2, text) or re.match(
+        pattern3, text)
 
 
 def read_file(file_path) -> str:
@@ -40,24 +35,22 @@ def result_output(count, path) -> None:
 
 def parse_args_console() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--read_file",
-        "-rf",
-        type=str,
-        help="path to read file")
-    parser.add_argument(
-        "--write_file",
-        "-wf",
-        type=str,
-        help="path to write file")
+    parser.add_argument("--read_file",
+                        "-rf",
+                        type=str,
+                        help="path to read file")
+    parser.add_argument("--write_file",
+                        "-wf",
+                        type=str,
+                        help="path to write file")
     return parser.parse_args()
 
 
 def main():
     args = parse_args_console()
 
-    write_path = args.write_file if (
-        args.write_file is not None) else "result.txt"
+    write_path = args.write_file if (args.write_file
+                                     is not None) else "result.txt"
 
     count_correct_numbers = 0
     if args.read_file is not None:
@@ -65,14 +58,15 @@ def main():
         text = text.splitlines()  # Превращаем текст в список строк
         with open(write_path, "w", encoding="utf-8") as wfile:
             for i in range(len(text)):  # Двигаемся по списку через индексы
-                if (correct_number(text[i])):  # Проверяем удовл. ли номер паттерну
+                if (correct_number(
+                        text[i])):  # Проверяем удовл. ли номер паттерну
+                    print(type(correct_number(text[i])))
                     count_correct_numbers += 1
                     # Нужно отойти на 4 строчки назад и 1 вперед, чтобы сохранить анкету
                     # с самого начала
                     wfile.write("\n" + str(count_correct_numbers) + ")\n")
                     for x in range(-4, 2):
                         wfile.write(text[i + x] + '\n')
-
 
         result_output(count_correct_numbers, write_path)
 
