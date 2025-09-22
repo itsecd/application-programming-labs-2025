e521856b1dc1629c22a7aa451c461e0cbaf36a4d
Ivan Barabanov <barabanov_vanechka06@mail.ru>
2025-09-22 10:08:10 +0300

    main декомпозирована на частичные ф-и

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import argparse
 import work_with_file
 
 
-def main():
+def argument_parsing() -> list[str]:
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', type=str, help='path to input file')
     parser.add_argument('output_file', type=str, help='path to output file')
     args = parser.parse_args()
-    data = work_with_file.read_file(args.input_file)
+    return [args.input_file, args.output_file]
+
+
+def data_process(data: str) -> list[str]:
     data_list = anketes.differentiate_by_anketes(data)
-    res = anketes.find_goal_anketes(data_list)
-    work_with_file.write_data(args.output_file, res)
-    print(len(res))
+    return anketes.find_goal_anketes(data_list)
+
+
+def main():
+    input_file, output_file = argument_parsing()
+    data = work_with_file.read_file(input_file)
+    result = data_process(data)
+    work_with_file.write_data(output_file, result)
+    print(len(result))
 
 
 if __name__ == "__main__":
