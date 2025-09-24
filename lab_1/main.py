c4252d856a49cc4ac7bbb7b6b82901af3d47bf82
Egor2034 <egor_2061@mail.ru>
2025-09-24 22:24:40 +0400

    Added the function for saving necessary forms

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,9 +3,8 @@ import argparse
 
 
 def read_file(filename: str) -> str:
-    """
-    Эта функция возвращает текст как одну большую строку
-    """
+    """Эта функция возвращает текст как одну большую строку"""
+
     try:
         with open(filename, "r", encoding="utf-8") as file:
             return file.read()
@@ -13,10 +12,20 @@ def read_file(filename: str) -> str:
         raise Exception("Ошибка при открытии файла")
 
 
+def write_file(filename: str, data: list[str]) -> None:
+    """Эта фунция выполняет запись найденных анкет в файл"""
+
+    with open(filename, "w", encoding="utf-8") as file:
+        count = 1
+
+        for item in data:
+            file.write(f"{count})\n{item}")
+            count += 1
+
+
 def args_parse() -> tuple[str, str]:
-    """
-    Эта функция парсит аргументы из консоли
-    """
+    """Эта функция парсит аргументы из консоли"""
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="Input file")
     parser.add_argument("-o", "--output", help="Output file")
@@ -54,8 +63,8 @@ def main() -> None:
     file, output = args_parse()
     text = read_file(file)
     result = forms_parse(text)
-    
-    print(result)
+    print(f"Найдено {len(result)} подходящих анкет")
+    write_file(output, result)
 
 
 if __name__ == "__main__":
