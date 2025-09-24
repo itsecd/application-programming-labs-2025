8fc2c687c9d9215e074919fcddcefcedf5dc93fb
Egor2034 <egor_2061@mail.ru>
2025-09-24 20:30:37 +0400

    Added the function for parsing arguments from console

--- lab_1/main.py
+++ lab_1/main.py
@@ -13,9 +13,23 @@ def read_file(path: str) -> str:
         raise Exception("Ошибка при открытии файла")
 
 
+def args_parse() -> tuple[str, str]:
+    """
+    Эта функция парсит аргументы из консоли
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Input file")
+    parser.add_argument("-o", "--output", help="Output file")
+    args = parser.parse_args()
+
+    if args.output is not None:
+        return (args.input, args.output)
+    else:
+        raise Exception("The name of ouput file can'b be None")
+
+
 def main() -> None:
     pass
-    
 
 if __name__ == "__main__":
     main()
