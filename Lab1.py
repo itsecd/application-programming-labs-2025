606ccaaff93cddf4bcc19f0e36c9cbdb582083a2
TimGaneev <tim.ganeev718@gmail.com>
2025-09-28 19:17:27 +0400

    Updated Lab1.py
    Улучшил работу функций, добавил обработку исключений

--- Lab1.py
+++ Lab1.py
@@ -1,21 +1,29 @@
 import argparse
 import re
 
-
-def parse_arguments():
+def parse_arguments() -> str:
     parser = argparse.ArgumentParser()
-    parser.add_argument("input", help="input file path")
-    parser.add_argument("output", help="output file path")
+    parser.add_argument("input", type=str, help="input file path")
     args = parser.parse_args()
-    return args
+    return args.input
 
 def read_file(name:str) -> str:
-    with open(name, "r", encoding="utf-8") as file:
-        return file.read()
+    print(f"Чтение файла {name}")
+    try:
+        with open(name, "r", encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Файл {name} не найден.")
+    except Exception as e:
+        raise Exception(f"Ошибка при чтении: {e}")
 
 def write_file(name:str, ovas:list[str]) -> None:
-    with open(name, "w", encoding="utf-8") as file:
-        file.write("\n\n".join(ovas))
+    try:
+        with open(name, "w", encoding="utf-8") as file:
+            file.write("\n\n".join(ovas))
+            print(f"Найденные анкеты сохранены в файл {name}")
+    except Exception as e:
+        raise Exception(f"Ошибка при записи: {e}")
 
 def find_ovas(data:str) -> list[str]:
     people = re.split(r'\n\n', data)
@@ -26,12 +34,15 @@ def find_ovas(data:str) -> list[str]:
     return ovas
 
 def main() -> None:
-    args = parse_arguments()
-    data = read_file(args.input)
-    ovas = find_ovas(data)
-
-    print(len(ovas))
-    write_file(args.output, ovas)
+    try:
+        input_file = parse_arguments()
+        data = read_file(input_file)
+        ovas = find_ovas(data)
+        print(f"Найдено {len(ovas)} анкет людей, чьи фамилии заканчиваются на 'ов(а)'")
+        if ovas:
+            write_file("ovas_profiles.txt", ovas)
+    except Exception as e:
+        print(f"Возникла ошибка: {e}")
 
 if __name__ == "__main__":
     main()
