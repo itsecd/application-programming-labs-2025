86713ad56c4de2dcc38d39c003d28fe08627c9a7
andruyxaParavoz <andrey.malahov.679@gmail.com>
2025-09-28 17:29:02 +0400

    Добавил обработку исключений

--- script.py
+++ script.py
@@ -4,27 +4,85 @@ from typing import List
 
 
 def read_file(file_path: str) -> str:
-    with open(file_path, "r", encoding="utf-8") as f:
-        return f.read()
+    """
+    Функция для чтения содержимого файла.
+
+    Args:
+        file_path (str): Путь к файлу для чтения
+
+    Returns:
+        str: Содержимое файла в виде строки
+
+    Raises:
+        FileNotFoundError: Если файл не найден
+        PermissionError: Если нет прав для чтения файла
+        UnicodeDecodeError: Если ошибка кодировки файла
+    """
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            return f.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Файл '{file_path}' не найден")
+    except PermissionError:
+        raise PermissionError(f"Нет прав для чтения файла '{file_path}'")
+    except UnboundLocalError:
+        raise UnicodeDecodeError(
+            "Ошибка кодировки файла."
+        )
 
 
 def write_file(file_path: str, data: List[str]) -> None:
-    with open(file_path, "w", encoding="utf-8") as f:
-        f.write("\n\n".join(data))
+    """
+    Записывает список анкет в файл.
+
+    Args:
+        file_path (str): Путь к входному файлу
+        data (List[str]): Список анкет для записи
+
+    Raises:
+        PermissionError: Если нет прав для записи в файл
+        IOError: Если произошла ошибка ввода-вывода
+    """
+    try:
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write("\n\n".join(data))
+    except PermissionError:
+        raise PermissionError(f"Нет прав для записи в файл '{file_path}'")
+    except IOError as e:
+        raise IOError(f"Ошибка записи в файл '{file_path}': {e}")
 
 
 def extract_ankets_with_invalid_email(content: str) -> List[str]:
+    """
+    Функция для извлечения анкет с некорректным email.
+
+    Args:
+        content (str): Содержимое файла с анкетами
+
+    Returns:
+        List[str]: список анкет с некорректными email
+
+    Raises:
+        ValueError: если содержимое файла имеет некорректный формат
+    """
+    if not content or not content.strip():
+        raise ValueError("Файл пуст")
+
     ALLOWED_DOMAINS = [
         'gmail.com',
         'mail.ru',
         'yandex.ru'
     ]
+
     valid_email_pattern = re.compile(
         r"\b[A-Za-z0-9._%+-]{1,64}"
         r"@(gmail\.com|mail\.ru|yandex\.ru)\b"
     )
 
-    ankets = re.split(r"\n\s*\n", content.strip())
+    try:
+        ankets = re.split(r"\n\s*\n", content.strip())
+    except re.error as e:
+        raise ValueError(f"Ошибка при чтении анкет: {e}")
 
     invalid_anktes = []
     for anket in ankets:
@@ -55,34 +113,75 @@ def extract_ankets_with_invalid_email(content: str) -> List[str]:
 
 def remove_ankets_with_invalid_email(
     content: str,
-        invalid_ankets: List[str]
+    invalid_ankets: List[str]
 ) -> List[str]:
-    ankets = re.split(r"\n\s*\n", content.strip())
+    """
+    Функция для удаления анкет с некорректным email
+
+    Args:
+        content (str): Содержимое файла с анкетами
+        invalid_ankets (List[str]): Список анкет для удаления
+
+    Returns:
+        List[str]: Список анкет без некорректных email
+
+    Raises:
+        ValueError: Если содержимое файла имеет некорректный формат
+    """
+    if not content or not content.strip():
+        raise ValueError("Файл пуст")
+
+    try:
+        ankets = re.split(r"\n\s*\n", content.strip())
+    except re.error as e:
+        raise ValueError(f"Ошибка при чтении анкет: {e}")
+
     invalid_set = set(invalid_ankets)
     return [anket for anket in ankets if anket not in invalid_set]
 
 
 def main() -> None:
-    if len(sys.argv) < 2:
-        print("Использование: python script.py <входной-файл> <выходной-файл>")
-        return
+    """
+    Основная функция программы.
 
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-    content = read_file(input_file)
-    invalid_ankets = extract_ankets_with_invalid_email(content)
+    Обрабатывает аргументы CLI, находит и удаляет анкеты с некорректыми email
+    """
+    try:
+        if len(sys.argv) < 3:
+            print("Использование:"
+                  " python script.py <входной-файл> <выходной-файл>")
+            return
 
-    print("Анкеты с некорректным email:")
-    print("=" * 80)
-    for i, anket in enumerate(invalid_ankets, 1):
-        print(anket)
+        input_file = sys.argv[1]
+        output_file = sys.argv[2]
+        content = read_file(input_file)
+        invalid_ankets = extract_ankets_with_invalid_email(content)
+
+        print("Анкеты с некорректным email:")
         print("=" * 80)
+        for i, anket in enumerate(invalid_ankets, 1):
+            print(anket)
+            print("=" * 80)
 
-    print(f"\nВсего найдено {len(invalid_ankets)} анкет с некорректным email.")
-    cleaned_ankets = remove_ankets_with_invalid_email(content, invalid_ankets)
-    write_file(output_file, cleaned_ankets)
-    print(f"\nУдалено {len(invalid_ankets)} анкет с некорректным  email.")
-    print(f"Сохранено {len(cleaned_ankets)} анкет в файл: {output_file}")
+        print(
+            f"\nНайдено {len(invalid_ankets)} анкет с некорректным email.")
+        cleaned_ankets = remove_ankets_with_invalid_email(
+            content, invalid_ankets)
+        write_file(output_file, cleaned_ankets)
+        print(f"\nУдалено {len(invalid_ankets)} анкет с некорректным  email.")
+        print(f"Сохранено {len(cleaned_ankets)} анкет в файл: {output_file}")
+    except FileNotFoundError as e:
+        print(f"Ошибка {e}")
+        sys.exit(1)
+    except PermissionError as e:
+        print(f"Ошибка прав доступа {e}")
+        sys.exit(1)
+    except ValueError as e:
+        print(f"Ошибка данных: {e}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Неизвестная ошибка: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
