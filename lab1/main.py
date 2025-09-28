16b02102d987edda7d172f5255ce23c4deeaa021
SlipenchukNikita <nikitaslipenchuk.70@mail.ru>
2025-09-28 20:39:16 +0400

    Добавил проверку согласно шаблону регулярного выражения

--- lab1/main.py
+++ lab1/main.py
@@ -1,12 +1,41 @@
 import argparse
 import re
 
+def readfile(filename: str) -> str:
+    try:
+        file = open(filename, "r")
+        print(f"File {filename} is parsed!")
+        text = file.read()
+        file.close()
+        return text
+    except FileNotFoundError:
+        print(f"Sorry, {filename} dont found!!!")
+        return ""
 
-parser = argparse.ArgumentParser()  # создание экземпляра парсера
-parser.add_argument('filename', type=str, help='filename')  # добавление позиционного аргумента командной строки
-args = parser.parse_args() # парсинг аргументов
-try:
-    file = open(args.filename)
-except FileNotFoundError:
-    print(f"Sorry, {args.filename} dont found!!!")
-print(f"File {args.filename} is parsed!")  # использование полученного значения аргумента
+def is_correct(date: str) -> bool:
+    if not re.fullmatch(r'\b(0?[1-9]|[12][0-9]|3[01])[/\-\.](0?[1-9]|1[0-2])[/\-\.]\d{4}\b', date):
+        print(date)
+        return False
+    else:
+        return True
+
+def main():
+    parser = argparse.ArgumentParser()  # создание экземпляра парсера
+    parser.add_argument('filename', type=str, help='filename')  # добавление позиционного аргумента командной строки
+    args = parser.parse_args() # парсинг аргументов
+    text = readfile(args.filename)
+    text = text.split("\n")
+    file = open("correct_date", "w")
+    count=0
+    for i in range(0,len(text),8):
+        if is_correct(text[i+4][15:]) == False:
+            count=count+1
+            print("yes")
+    print(count)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
