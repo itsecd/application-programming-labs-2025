c5ea0504d570ef2f7d642560233d1c5804f8cb1b
SlipenchukNikita <nikitaslipenchuk.70@mail.ru>
2025-09-29 14:06:53 +0400

    Доделал все функции, но не раскидал по файлам

--- lab1/main.py
+++ lab1/main.py
@@ -1,4 +1,5 @@
 import argparse
+import io
 import re
 
 def readfile(filename: str) -> str:
@@ -13,28 +14,34 @@ def readfile(filename: str) -> str:
         return ""
 
 def is_correct(date: str) -> bool:
-    if not re.fullmatch(r'\b(0?[1-9]|[12][0-9]|3[01])[/\-\.](0?[1-9]|1[0-2])[/\-\.]\d{4}\b', date):
-        print(date)
-        return False
+    if not re.fullmatch(r'(0?[0-9]|[12][0-9]|[3][01])[./-]+(0?[0-9]|[1][012])[./-]+([1][9][0-9]{2}|[2][0][01][0-9]|[2][0][2][0-5])', date):
+            return False
     else:
-        return True
+            return True
+
+def print_men(mens: list[str], index:int) -> None:
+    for i in range(index,index+8):
+            print(mens[i])
+
+def to_file(file: io.TextIOWrapper, index: int, mens: list[str]) -> None:
+     for i in range(index, index + 8):
+            file.write(mens[i])
+            file.write("\n")
+     file.write("\n")
 
 def main():
     parser = argparse.ArgumentParser()  # создание экземпляра парсера
     parser.add_argument('filename', type=str, help='filename')  # добавление позиционного аргумента командной строки
     args = parser.parse_args() # парсинг аргументов
     text = readfile(args.filename)
-    text = text.split("\n")
-    file = open("correct_date", "w")
-    count=0
-    for i in range(0,len(text),8):
-        if is_correct(text[i+4][15:]) == False:
-            count=count+1
-            print("yes")
-    print(count)
-
-
-
+    mens = text.split("\n")
+    mens.append("")
+    file = open("correct_date.txt", "w")
+    for i in range(0,len(mens),8):
+        if not is_correct(mens[i+4][15:]):
+            print_men(mens,i)
+        else:
+            to_file(file,i,mens)
 
 
 if __name__ == "__main__":
