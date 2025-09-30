4f4bfb82844d6656ee74bdccf077ad21c501ccfe
Mavin <mavinchertov@gmail.com>
2025-09-30 19:10:52 +0400

    Обновил регулярное выражение, доработал мелочи

--- lab1.py
+++ lab1.py
@@ -1,14 +1,14 @@
 import re
 import argparse
 
-with open("data.txt", "r", encoding="utf-8") as file :
+parser = argparse.ArgumentParser()
+parser.add_argument("input", type=str, help="input file path")
+args = parser.parse_args()
+
+with open(args.input, "r", encoding="utf-8") as file :
     text = file.read()
 
-pattern = "[A-Za-z0-9._%+-]+@[gmail.com|mail.ru|yandex.ru]+"
-emails = re.findall(pattern, text)
-print(emails)
-print(len(emails))
-
+pattern = r"[A-Za-z0-9._%+-]+@(?:gmail\.com|mail\.ru|yandex\.ru)"
 valid_forms = []
 
 forms = text.split("\n\n")
@@ -18,4 +18,6 @@ for form in forms :
 with open("valid_forms.txt", "w", encoding="utf8") as output :
     for form in valid_forms:
         output.write(form + "\n\n")
+print(f"Количество анкет с верно указанными почтами: {len(valid_forms)}")
+print("Данные анкеты сохранены в файл valid_forms.txt")
 
