12b803e8de505150824ffc434529a3443789ea44
zaripov-damir <damirzari@bk.ru>
2025-10-06 11:39:50 +0300

    Add filename_result arg main.py

--- lab1-var24/main.py
+++ lab1-var24/main.py
@@ -1,14 +1,13 @@
 import func
 
 def main():
-    filename = func.get_args()
+    filename, filename_result = func.get_args()
     content = func.read_file(filename)
     
-    if (len(content) == 0) or (content is None):
+    if not content:
         print("Error: file is empty or data is corrupted.")
         exit(1)
 
-    filename_result = "result.txt"
     func.process_people(content, filename_result)
     print(f"Result was recorded to {filename_result}")
 
