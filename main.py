8f42ec9e67e85193596ae48ae3e2a97458b13d8d
Elisaveta06 <elizaveta.arbi06@mail.ru>
2025-10-08 13:08:37 +0400

    Добавила исключения в read_file и аргументы командной строки.

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 import re
+import sys
+import argparse
 
-
-def read_file() -> str:
-    filename = "C:/Users/elisa/Downloads/data.txt"
-    with open(filename, "r", encoding='utf-8') as file:
-        text = file.read()
-    return text
+def read_file(filename: str) -> str | None:
+    try:
+        with open(filename, "r", encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"Ошибка: Файл '{filename}' не найден.")
+        return None
+    except Exception as e:
+        print(f"Ошибка при чтении файла: {e}")
+        return None
 
 def main():
-    read_file()
+    parser = argparse.ArgumentParser(
+        description="Извлечение анкет людей, родившихся в 21 веке, из файла data.txt и сохранение их в новый файл."
+    )
+    parser.add_argument(
+        'filename', 
+        type=str,
+        help='Имя исходного файла для обработки.'
+    )
+    parser.add_argument(
+        '-o', '--output', 
+        type=str, 
+        default='born_in_21st_century.txt', 
+        help='Имя выходного файла для сохранения результатов.'
+    )
+    parser.add_argument(
+        '-v', '--verbose', 
+        action='store_true', 
+        help='Вывод на экран анкеты людей, родившихся в 21 веке.'
+    )
+    
+    args = parser.parse_args()
 
 if __name__ == "__main__":
     main()
