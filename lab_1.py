9e0c0726e3e0ec37e0658b522be5b766ea5d6761
Chuqaz <kardanovmr52@mail.ru>
2025-10-09 08:27:20 +0300

    Изменение форматирования, декомпозиция кода

--- lab_1.py
+++ lab_1.py
@@ -1,47 +1,64 @@
-import re
 import argparse
+import re
 
-def open_file(filename_1:str,filename_2:str):
+
+def read_file(filenamedata: str) -> str:
     """
     открытие файла
     """
     try:
-        with open(filename_1, "r", encoding="utf-8") as file:
-            text = read_file(file,filename_2)
-            return text
+        with open(filenamedata, "r", encoding="utf-8") as file:
+            text_file = file.read()
+            return text_file
     except FileNotFoundError:
         raise FileNotFoundError("Файл не найден")
 
-def read_file(file,filename_2:str):
+
+def search_moscow_ankets(text_file: str) -> list[str]:
     """
-    чтение содержимого и
+    поиск нужных анкет
+    """
+    ankets = re.findall(r"\d+\)\n(?:.*\n)+?(?=\d+\)|$)", text_file)
+    moscow = []
+    for anketa in ankets:
+        if re.search(r"Город:\s*[г.\s*]Москва", anketa):
+            moscow.append(anketa)
+    return moscow
+
+
+def write_data(filenameoutput: str, Moscow_list: list) -> None:
+    """
     запись в другой файл
     """
-    content=file.read()
-    ankets = re.findall(r'\d+\)\n(?:.*\n)+?(?=\d+\)|$)', content)
-    moscow=[]
-    for anketa in ankets:
-        if re.search(r"Город:\s*[г.\s*]Москва",anketa):
-            moscow.append(anketa)
-    quantity=len(re.findall("Москва",content))
-    with open(filename_2, "w", encoding="utf-8") as file_1:
-        for anketa in moscow:
+    with open(filenameoutput, "w", encoding="utf-8") as file_1:
+        for anketa in Moscow_list:
             file_1.write(anketa)
-    return quantity
 
-def parsing():
+
+def parsing() -> tuple[str, str]:
     """
     передача аргументов через командную строку
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', type=str,help="Введите путь файла")
-    parser.add_argument('filenameoutput', type=str,help="Введите путь файла для вывода")
-    args= parser.parse_args()
-    return args.filename, args.filenameoutput
+    parser.add_argument("filenamedata", type=str, help="Введите путь файла")
+    parser.add_argument(
+        "filenameoutput", type=str, help="Введите путь файла для вывода"
+    )
+    args = parser.parse_args()
+    return args.filenamedata, args.filenameoutput
 
-def main()->None:
-    filename_1,filename_2=parsing()
-    print(open_file(filename_1,filename_2))
+
+def main() -> None:
+    try:
+        filename_data, filename_output = parsing()
+        text_file = read_file(filename_data)
+        moscow_ankets = search_moscow_ankets(text_file)
+        print(len(moscow_ankets))
+        write_data(filename_output, moscow_ankets)
+
+    except Exception as exp:
+        print(exp)
+
 
 if __name__ == "__main__":
     main()
