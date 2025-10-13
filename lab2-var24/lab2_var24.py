2b378f0d5a05657b7eb8d2e15fb4145870b6781f
Povelitel-kiber-ezikov <mr.timur1705@gmail.com>
2025-10-13 19:33:45 +0400

    добавил функции генерирования url адресса и чтение чтраницы по этому адрессу

--- lab2-var24/lab2_var24.py
+++ lab2-var24/lab2_var24.py
@@ -1,6 +1,8 @@
 import csv
-
-
+import random
+import argparse
+import requests
+from bs4 import BeautifulSoup
 
 
 CSV_HEADER = ["genre", "abs_path", "real_path", "url", ]
@@ -24,3 +26,18 @@ def append_to_csv(csv_path, genre, abs_path, real_path, url) -> None:
         writer = csv.writer(file)
         writer.writerow([genre, abs_path, real_path, url])
 
+def generate_url(genre) -> str:
+    """
+    создаю ссылку на страницу жанра в mixkit
+    """
+    return f"https://mixkit.co/free-stock-music/{genre}/"
+
+def get_html(url) -> str:
+    """
+    получаю страницу
+    """
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
+    resp = requests.get(url, headers= headers)
+    if resp.ok:
+        return resp.text
+        
