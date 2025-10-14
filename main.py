84e3dc1a6f78de788c203fd1e464c48116ba84a0
gar1998 <gareevadel262825@yandex.ru>
2025-10-14 11:59:54 +0400

    Удалил print в функцие write_in_file. Осуществил ввод пути файла для записи через командную строку.

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import argparse
 import re
 import sys
 
-def parse_command_line_arguments() -> str:
+def parse_command_line_arguments() -> tuple[str, str]:
     """
     Разбирает аргументы командной строки
     Returns: имя файла с анкетами
     """
     parser = argparse.ArgumentParser(exit_on_error=False)
     parser.add_argument("input_file", type=str, help = "File_name")
+    parser.add_argument("output_file", type=str, help="File_name")
     try:
         args = parser.parse_args()
-        return args.input_file
+        return args.input_file, args.output_file
     except argparse.ArgumentError as e:
         raise ValueError(f"Ошибка при разборе аргументов командной строки: {e}")
 
@@ -64,17 +65,17 @@ def filter_profiles(last_name_pattern: re.Pattern, list_of_profiles: list[list[s
             matching_profiles.append(profile)
     return matching_profiles
 
-def write_in_file(matching_profiles: list[list[str]]) -> None:
+def write_in_file(matching_profiles: list[list[str]], filename: str) -> None:
     """
     Запись найденных анкет в файл
     :param matching_profiles: список анкет, удовлетворяющих валидному формату фамилии
+    :param filename: имя файла
     :return: ничего
     """
     try:
-        with open("New file.txt", 'w', encoding='utf-8') as file:
+        with open(filename, 'w', encoding='utf-8') as file:
             for profile in matching_profiles:
                 file.write("".join(profile) + "\n")
-        print(f"Результат сохранен: Анкеты найденных людей сохранены в файл New file.")
     except IOError as e:
         raise IOError(f"Ошибка при записи файла 'New file.txt': {e}")
     except Exception as e:
@@ -83,10 +84,10 @@ def write_in_file(matching_profiles: list[list[str]]) -> None:
 def main() -> None:
     # Получение имени файла с анкетами из командной строки
     try:
-        file_name = parse_command_line_arguments()
+        file_names = parse_command_line_arguments()
 
         # Чтение файла по строчно
-        lines = read_file(file_name)
+        lines = read_file(file_names[0])
 
         # Формирование списка анкет
         list_of_profiles = create_list_of_profiles(lines)
@@ -101,7 +102,7 @@ def main() -> None:
         print(f"Найдено: {len(matching_profiles)} человек с фамилиями, оканчивающимися на 'ов' или 'ова'.")
 
         # Запись найденных анкет в новый файл
-        write_in_file(matching_profiles)
+        write_in_file(matching_profiles, file_names[1])
 
     except ValueError as e:
         print(f"Ошибка запуска: {e}")
