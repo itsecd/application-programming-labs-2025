71000fe6fb04f706ee0deb8c3d32653c2dbb04a3
andrey-shishkin19 <av.shishkin2006@gmail.com>
2025-10-19 20:08:57 +0300

    Создал lab1.py. Начал работу.

---
+++ lab1.py
@@ -0,0 +1,13 @@
+import re
+import argparse
+from typing import List, Dict, Tuple
+
+
+def main() -> None:
+    """
+    Основная функция программы.
+    """
+
+
+if __name__ == "__main__":
+    main()
