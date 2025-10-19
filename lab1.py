cf3ff1df3f3d8eb274f19d19f088cab263b9774d
Mavin <mavinchertov@gmail.com>
2025-10-19 18:49:19 +0400

    Выходной файл через аргумент, PEP8

--- lab1.py
+++ lab1.py
@@ -1,14 +1,17 @@
 import re
 import argparse
 
+
 def input_argument():
     """
     С помощью аругмента забирает путь к исходному файлу
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("input", type=str, help="input file path")
+    parser.add_argument("output", type=str, help="output file path")
     args = parser.parse_args()
     return args
+
 
 def read_file(file_path):
     """
@@ -17,6 +20,7 @@ def read_file(file_path):
     with open(file_path, "r", encoding="utf-8") as file :
         text = file.read()
     return text
+
 
 def form_with_email(text):
     """
@@ -30,21 +34,24 @@ def form_with_email(text):
             valid_forms.append(form)
     return valid_forms
 
-def write_to_file(valid_forms):
+
+def write_to_file(valid_forms, output_path):
     """
     Записывает подходящие анкеты в отдельный файл
     """
-    with open("valid_forms.txt", "w", encoding="utf8") as output :
+    with open(output_path, "w", encoding="utf8") as output :
         for form in valid_forms:
             output.write(form + "\n\n")
+
 
 def main():
     args = input_argument()
     text = read_file(args.input)
     valid_forms = form_with_email(text)
-    write_to_file(valid_forms)
+    write_to_file(valid_forms, args.output)
     print(f"Количество анкет с верно указанной почтой: {len(valid_forms)}")
-    print("Отобранные анкеты сохранены в файл valid_forms.txt")
+    print(f"Отобранные анкеты сохранены в {args.output}")
+
 
 if __name__ == '__main__':
     main()
