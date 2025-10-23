0dbb8f639488ec3f28a540dce9d86482153bd32b
BlaZeriTUwU <schumakownikita@gmail.com>
2025-10-23 19:12:41 +0400

    Добавил изменения

--- laba_3/audio_processor.py
+++ laba_3/audio_processor.py
@@ -1,16 +1,19 @@
 import numpy as np
 import soundfile as sf
 
-def transformations(source: str, music: str, alpha: int) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+
+def transformations(
+    source: str, music: str, alpha: int
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Функция увеличивает музыку в alpha раз
     """
     data, samplerate = sf.read(source)
     data_sped = data[::alpha]
-    
+
     t_orig = np.arange(len(data)) / samplerate
     t_sped = np.arange(len(data_sped)) / samplerate
-    
+
     sf.write(music, data_sped, samplerate, format="WAV")
-    
+
     return data, data_sped, t_orig, t_sped
--- laba_3/main.py
+++ laba_3/main.py
@@ -1,27 +1,41 @@
+import argparse
 import os
 import sys
+
 from config import logger
-from cli import parser_t
 from audio_processor import transformations
 from visualizer import plot_audio
+
+def parser_t() -> tuple[str, str, str, int]:
+    """
+    Позволяет через консоль запускать код с аргументами
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", type=str, help="Путь к файлу для работы с ним")
+    parser.add_argument("output_file", type=str, help="Путь для сохранения картинки")
+    parser.add_argument("music", type=str, help="Путь для сохранения музыки")
+    parser.add_argument("alpha", type=int, help="Число для ускорения")
+    args = parser.parse_args()
+    return args.source, args.output_file, args.music, args.alpha
+
 
 def main():
     try:
         source, output_plot, output_audio, alpha = parser_t()
-        
+
         if alpha < 1:
             raise ValueError("alpha должен быть ≥ 1")
-        
+
         size = os.path.getsize(source)
         logger.info(f"Размер файла: {size} байт")
-        
+
         data, data_sped, t_orig, t_sped = transformations(source, output_audio, alpha)
         plot_audio(data, data_sped, t_orig, t_sped, alpha, output_plot)
-        
+
     except Exception as e:
         logger.error(e)
         sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
