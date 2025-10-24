ea599fe8dd03196157d203f40c135e25fae6c19b
trueHyper <volkoofsenya@gmail.com>
2025-10-24 12:34:27 -0700

    add: функция, которая достает ссылки и названия песен

--- lab2/main.py
+++ lab2/main.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
+from typing import List, Dict
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -14,17 +16,50 @@ def fetch_page(url: str, timeout: int = 2) -> str | None:
     except Exception:
         return None
 
+def extract_tracks_from_html(html: str) -> List[Dict[str, str]]:
+    """
+    output:
+        [
+            {"title": "song name", "mp3_link": "https://..."},
+            ...
+        ]
+    """
+    soup = BeautifulSoup(html, "html.parser")
+
+    # контейнер со всеми блоками песен на странице
+    # div.item-grid__items > [div.item-grig__item, ...]
+    container = soup.select_one("div.item-grid__items")
+    if not container:
+        return [] 
+
+    tracks = []
+
+    for item in container.select("div.item-grid__item"):
+
+        title_tag = item.select_one("h2.item-grid-card__title")
+        player_tag = item.select_one('div[data-test-id="audio-player"]')
+
+        if title_tag and player_tag:
+            
+            tracks.append({
+                "title": title_tag.get_text(strip=True),
+                "mp3_link": player_tag.get("data-audio-player-preview-url-value"),
+            })
+
+    return tracks
+
 def main():
-    url = "https://mixkit.co/free-stock-music/instrument/trumpet/"
+    url = "https://mixkit.co/free-stock-music/instrument/acoustic-guitar/"
     
     html = fetch_page(url)
     
     if html:
         print("✅OK")
         print("LEN HTML:", len(html))
-        print(html[:500])
     else:
         print("⚠️ERROR")
+
+    print(extract_tracks_from_html(html))
 
 if __name__ == "__main__":
     main()
