bf2534f1ad7dd4646af6bb3b958c33dc2e6e5260
trueHyper <volkoofsenya@gmail.com>
2025-10-26 14:53:37 -0700

    lab5

---
+++ lab5/track_iterator.py
@@ -0,0 +1,40 @@
+import os
+import csv
+from typing import Iterator
+
+class TrackIterator:
+    def __init__(self, source: str):
+        self.paths = []
+        if os.path.isdir(source):
+            for root, _, files in os.walk(source):
+                for f in files:
+                    if f.endswith(".mp3"):
+                        self.paths.append(os.path.join(root, f))
+        elif os.path.isfile(source) and source.endswith(".csv"):
+            with open(source, encoding="utf-8") as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    self.paths.append(row["absolute_path"])
+        else:
+            raise ValueError("The source must be the path to the csv or folder")
+        self._index = 0
+
+    def __iter__(self) -> Iterator[str]:
+        return self
+
+    def __next__(self) -> str:
+        if self._index >= len(self.paths):
+            raise StopIteration
+        path = self.paths[self._index]
+        self._index += 1
+        return path
+
+    def previous(self):
+        if self._index > 1:
+            self._index -= 2
+            return self.__next__()
+        return None
+
+    def reset(self):
+        self._index = 0
+
