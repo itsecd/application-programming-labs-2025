582564b1481c5c9ababdf6bb47d3a14f062dd41b
lamtiaf <lamtiaf@mail.ru>
2025-10-27 04:36:12 +0400

    Added exception throwing

--- lab1.py
+++ lab1.py
@@ -8,10 +8,16 @@ def parse_arguments() -> str:
     return args.input_file
 
 def read_file(input_file: str) -> str:
-
     print(f"Reading file: {input_file}")
-    with open(input_file, 'r', encoding='utf-8') as file:
-        return file.read()
+    try:
+        with open(input_file, 'r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File '{input_file}' not found")
+    except PermissionError:
+        raise PermissionError(f"No permission to read file '{input_file}'")
+    except Exception as exc:
+        raise Exception(f"Error reading file: {exc}")
 
 def main() -> None:
     try:
@@ -19,8 +25,12 @@ def main() -> None:
         data = read_file(input_file)
         print(data)
         
-    except Exception as exc:
-        print(f"Возникла ошибка: {exc}")
+    except FileNotFoundError as e:
+        print(f"File error: {e}")
+    except PermissionError as e:
+        print(f"Permission error: {e}") 
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     main()
