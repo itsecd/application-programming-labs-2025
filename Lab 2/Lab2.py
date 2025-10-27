985b409c72306322c048f28f8fea4bbceb623681
TimGaneev <tim.ganeev718@gmail.com>
2025-10-27 19:37:52 +0400

    Добавил итератор, начал функцию выбора цветов

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -3,6 +3,25 @@ import csv
 import os
 from icrawler.builtin import GoogleImageCrawler
 
+
+class PathsIterator:
+    def __init__(self, path: str):
+        with open(path, newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            self.paths = [row for row in reader]
+            self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count < len(self.paths):
+            self.count += 1
+            return self.paths[self.count]
+        else:
+            raise StopIteration
+
+
 # def parse_arguments() -> str:
 #     """
 #     Парсинг аргументов из командной строки
@@ -13,9 +32,36 @@ from icrawler.builtin import GoogleImageCrawler
 #     return args.input
 
 
-def turtle_crawler(path: str, num: int) -> None:
+def colors_choice() -> list[str]:
+    user_choice = -1
+    while user_choice != 0:
+        colors = list[str]()
+        print(
+            "\n\nВыберите цвета изображений черепах:\n"
+            "1)Green\n"
+            "2)Blue\n"
+            "3)Yellow\n"
+            "4)Brown\n"
+            "5)Grey\n"
+            "6)Black&White\n"
+            "0)Готово\n\n"
+            "Выбраны: "
+        )
+        for i in colors:
+            print(i+", ")
+        user_choice = int(input("\nВаш выбор: "))
+        if user_choice == 1:
+            if colors.__contains__("green"):
+                colors.remove("green")
+            else:
+                colors.append("green")
+    return colors
+
+
+def turtle_crawler(path: str, num: int, colors: list[str]) -> None:
     google_crawler = GoogleImageCrawler(storage={"root_dir": path})
-    google_crawler.crawl(keyword="turtle", max_num=num)
+    for i in colors:
+        google_crawler.crawl(keyword="turtle " + i, max_num=num//len(colors))
 
 
 def get_paths(dir_path: str) -> list[[str]]:
@@ -39,12 +85,16 @@ def main() -> None:
         img_path = "imgs"
         annotation_path = "annotation.csv"
         num = 3
+        colors = colors_choice()
         if not os.path.exists(img_path):
             os.mkdir(img_path)
-        turtle_crawler(img_path, num)
+        turtle_crawler(img_path, num, colors)
         table = get_paths(img_path)
-        #print(table)
+        print(table)
         annotation_maker(annotation_path, table)
+        # iter = PathsIterator(annotation_path)
+        # for i in iter:
+        #     print(i)
     except Exception as exc:
         print(f"Возникла ошибка: {exc}")
 
