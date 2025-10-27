1d7f89724edc53166434b323738950edfa8b126e
lamtiaf <lamtiaf@mail.ru>
2025-10-27 04:16:28 +0400

    Add basic file reading and CLI arguments

---
+++ lab1.py
@@ -0,0 +1,26 @@
+﻿import argparse
+import re
+
+def parse_arguments() -> str:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str, help="input file path")
+    args = parser.parse_args()
+    return args.input_file
+
+def read_file(input_file: str) -> str:
+
+    print(f"Reading file: {input_file}")
+    with open(input_file, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def main() -> None:
+    try:
+        input_file = parse_arguments()
+        data = read_file(input_file)
+        print(data)
+        
+    except Exception as exc:
+        print(f"Возникла ошибка: {exc}")
+
+if __name__ == "__main__":
+    main()
