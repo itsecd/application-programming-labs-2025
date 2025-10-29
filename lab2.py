8a7e952ad7b5dfb7ad69c5df3f4f5fe717d9f813
MinhNhat236 <minhnhat236@gmail.com>
2025-10-29 15:40:10 +0400

    update lab2

--- lab2.py
+++ lab2.py
@@ -5,8 +5,10 @@ import os
 import argparse
 from typing import List, Iterator
 
-
 class FileIterator:
+    """
+    Итератор
+    """
     def __init__(self, paths: List[str]) -> None:
         self.paths = paths
         self.index = 0
@@ -24,22 +26,52 @@ class FileIterator:
     
 
 def csv_init(csv_path: str) -> None:
+    """
+    Создает новый CSV-файл с заголовками.
+    Параметры:
+        csv_path (str): путь к CSV-файлу.
+    """
     with open(csv_path, "w", encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(["abs_path", "rel_path", "url"])
 
-
 def append_to_csv(csv_path: str, abs_path: str, rel_path: str, url: str) -> None:
+    """
+    Добавляет одну строку в CSV-файл с данными о скачанном файле.
+    Параметры:
+        csv_path (str): путь к CSV-файлу.
+        abs_path (str): абсолютный путь к файлу.
+        rel_path (str): относительный путь к файлу.
+        url (str): ссылка на источник файла.
+    """
     with open(csv_path, "a", encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         writer.writerow([abs_path,rel_path,url])
 
+
 def get_html(url: str) -> str:
+    """
+    Загружает HTML-код страницы по указанному URL.
+    Параметры:
+        url (str): адрес веб-страницы.
+
+    Возвращает:
+        str: текст HTML-документа.
+    """
     headers = {"User-Agent": "Mozilla/5.0"}
     resp = requests.get(url, headers=headers)
     return resp.text
 
+
 def mp3_parse(html: str) -> List[str]:
+    """
+    Парсит HTML и извлекает все ссылки на mp3-файлы.
+    Параметры:
+        html (str): HTML-код страницы.
+
+    Возвращает:
+        List[str]: список URL-адресов mp3-файлов.
+    """
     soup = BeautifulSoup(html, "html.parser")
     urls = []
     for div in soup.find_all("div", attrs={"data-audio-player-preview-url-value": True}):
@@ -47,16 +79,30 @@ def mp3_parse(html: str) -> List[str]:
     return list(dict.fromkeys(urls))
 
 def download_mp3(url: str, path: str) -> None:
+    """
+    Скачивает mp3-файл по указанному URL и сохраняет его на диск.
+    Параметры:
+        url (str): ссылка на mp3-файл.
+        path (str): путь для сохранения файла.
+    """
     try:
         r = requests.get(url, stream = True)
         with open(path, "wb") as file:
-            for chunk in r.inter_content(8192):
+            for chunk in r.iter_content(8192):
                 if chunk:
                     file.write(chunk)
     except Exception as error:
         print("Ошибка при загрузке:", url, error)
 
+
 def process_transport(output_dir: str, csv_path: str) -> None:
+    """
+    Загружает и сохраняет звуки категории 'Transport'.
+
+    Параметры:
+        output_dir (str): папка для сохранения mp3-файлов.
+        csv_path (str): путь к создаваемому CSV-файлу
+    """
     url = "https://mixkit.co/free-sound-effects/transport/"
     print("Загрузка данных с сайта:", url)
     html = get_html(url)
@@ -83,6 +129,14 @@ def process_transport(output_dir: str, csv_path: str) -> None:
     print("Завершено! CSV-файл сохранён по пути", csv_path)
 
 def main():
+    """
+    Точка входа программы. 
+    Обрабатывает аргументы командной строки и запускает основную функцию.
+    
+    Аргументы командной строки:
+        --output_dir : каталог для сохранения файлов.
+        --csv_path   : путь к файлу аннотации (CSV).
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--output_dir", required=True, help="Папка для сохранения mp3-файлов")
     parser.add_argument("--csv_path", required=True, help="Путь к CSV-файлу аннотации")
