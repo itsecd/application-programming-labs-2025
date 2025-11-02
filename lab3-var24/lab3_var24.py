912af493af7a5a75fe940a7eb9b9824adb9abf76
Povelitel-kiber-ezikov <mr.timur1705@gmail.com>
2025-11-02 16:31:33 +0400

    добавил функцию создания эха

--- lab3-var24/lab3_var24.py
+++ lab3-var24/lab3_var24.py
@@ -1,7 +1,19 @@
-#import numpy
-#import soundfile as sf
-#import matplotlib.pyplot as plt
+import numpy
+import soundfile as sf
+import matplotlib.pyplot as plt
 import os
 
-print("hello world")
+def load_music(path:str) ->  np.ndarray:
+    data, samplerate = sf.read(filepath)
+    return data
+
+def create_echo(audio_data: np.ndarray, delay_samples: int, decay: float) -> np.ndarray:
+    output_size = len(audio_data) + delay_samples
+    result = np.zeros(output_size, dtype=audio_data.dtype)
+    
+    result[:len(audio_data)] = audio_data
+    
+    result[delay_samples:delay_samples + len(audio_data)] += audio_data * decay
+    
+    return result
 
