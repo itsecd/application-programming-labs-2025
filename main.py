7ad93204aef8dddf014d49a030e155797ea40910
Pavel-Gornov <gornovpavel30@yandex.ru>
2025-11-04 23:44:46 +0400

    Финальные штрихи

--- main.py
+++ main.py
@@ -38,9 +38,10 @@ def main(file_path: str, output_path: str) -> None:
         file_path (str): Путь до обрабатываемого файла
         output_path (str): Путь для сохранения результата
     """
-
-    data, samplerate = sf.read(file_path)
+    with open(f"{file_path}", mode="rb") as f:
+        data, samplerate = sf.read(f)
     print(f"Частота дискретизации: {samplerate} Гц.")
+    print(f"Размер аудио: {data.shape}")
     noise = np.random.normal(loc=0, scale=0.02, size=(data.shape)) # Массив случайных значений шума.
     noise_data = data + noise
     sf.write(output_path, noise_data, samplerate)
@@ -50,7 +51,7 @@ def main(file_path: str, output_path: str) -> None:
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", type=str, help="Аудиофайл для обработки")
+    parser.add_argument("-f", "--file", type=str, help="Аудиофайл для обработки", default="downloads/violin/43.mp3")
     parser.add_argument("-o", "--out", type=str, help="Путь для сохранения результата", default="out.mp3")
     args = parser.parse_args()
 
