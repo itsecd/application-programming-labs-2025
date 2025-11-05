45fd53b748ac10d2ff96cf496fb8763217aea333
kortunova <kortunova2006@mail.ru>
2025-11-05 09:10:14 +0400

    оставлю в таком варианте с функцией load_image_alternative тк без вопросов открывается изображение

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import argparse
+from typing import Optional
+
 import cv2
+import numpy as np
 from image_processor import create_puzzle
 from visualizer import show_comparison
+
+
+def load_image_alternative(image_path: str) -> Optional[np.ndarray]:
+    """Альтернативный способ загрузки изображения(из-за проблем с кодировкой)"""
+    try:
+        img = cv2.imread(image_path)
+        if img is not None:
+            return img
+        
+        with open(image_path, 'rb') as f:
+            file_bytes = np.frombuffer(f.read(), np.uint8)
+            img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+            return img
+    except Exception as e:
+        print(f"Ошибка загрузки: {e}")
+        return None
 
 
 def main() -> None:
@@ -13,7 +32,7 @@ def main() -> None:
     args = parser.parse_args()
     
     try:
-        original_img = cv2.imread(args.input)
+        original_img = load_image_alternative(args.input)
         if original_img is None:
             print(f"Не удалось загрузить изображение: {args.input}")
             return
