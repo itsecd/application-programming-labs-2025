541bf39a99a2312cde641a6ae5879394b5f16129
Chuqaz <kardanovmr52@mail.ru>
2025-11-06 08:44:50 +0300

    Изменение согласно требованиям change_image.py

--- change_image.py
+++ change_image.py
@@ -1,10 +1,17 @@
 import cv2
 import numpy as np
 
-def change_image(img: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+
+def bgr_2_rgb(img: np.ndarray) -> np.ndarray:
+    """
+    Конвертирование изображения для корректного отображения
+    """
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+def change_image(image_rgb: np.ndarray) -> np.ndarray:
     """
     Изменение изображения
     """
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_changed = img_rgb[:, :, [2, 1, 0]]
-    return img_rgb, img_changed
+    image_changed = image_rgb[:, :, [2, 1, 0]]
+    return image_changed
