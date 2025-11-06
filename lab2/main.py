7ca142bbeaa63a66d87903737012fe634bea829f
clayw <clayworks11337@gmail.com>
2025-11-06 23:43:48 +0400

    Создал функцию для получения ключевого слова

--- lab2/main.py
+++ lab2/main.py
@@ -1,1 +1,21 @@
+import csv
+from icrawler.builtin import GoogleImageCrawler
+import requests
 
+def get_keyword() -> str:
+	"""
+	Получение от пользователя ключевых слов
+	"""
+	keyword = input("Введите ключевое слово: ")
+	return keyword
+
+def main() -> None:
+	try:
+		keyword1 = "dog" # заменить потом
+		keyword2 = "cat" # заменить потом
+		print(keyword1, keyword2)
+	except Exception as exc:
+		print(f"Возникла ошибка: {exc}")
+
+if __name__ == "__main__":
+	main()
