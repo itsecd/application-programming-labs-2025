4c370867739db61565641cf1475e05e426f0f629
kopcevalera58-dev <kopcevalera58@gmail.com>
2025-11-08 15:28:15 +0500

    Добавлен парсинг аргументов командной строки

---
+++ func.py
@@ -0,0 +1,31 @@
+import argparse
+import re
+
+
+def get_args():
+    filenames = []
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str)
+    parser.add_argument("output_file", type=str)
+    args = parser.parse_args()
+    filenames.append(args.input_file)
+    filenames.append(args.output_file)
+    return filenames
+
+
+def read_file(filename: str) -> str:
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        return ""
+
+
+def is_valid_email(contact: str) -> bool:
+    pattern = r'^[a-zA-Z0-9._%+-]+@(gmail\.com|mail\.ru|yandex\.ru)$'
+    return bool(re.match(pattern, contact))
+
+
+def write_emails(file, emails_list):
+    for email in emails_list:
+        file.write(email + "\n")
