10a1e65bf6ed7dd0a17edc527036e5113018929c
meow <rinegeto@gmail.com>
2025-11-09 17:06:19 +0400

    также добавила исключения в main.py

--- lab1/main.py
+++ lab1/main.py
@@ -7,15 +7,19 @@ def main() -> None:
     parser.add_argument("filename", type=str, help="Путь к файлу с анкетами")
     args = parser.parse_args()
 
-    content = read_file(args.filename)
-    profiles = parse_profiles(content)
+    try:
+        content = read_file(args.filename)
+        profiles = parse_profiles(content)
 
-    oldest, youngest = find_oldest_and_youngest(profiles)
+        oldest, youngest = find_oldest_and_youngest(profiles)
 
-    print("Самый старый человек:\n")
-    print(format_profile(oldest))
-    print("\nСамый молодой человек:\n")
-    print(format_profile(youngest))
+        print("Самый старый человек:\n")
+        print(format_profile(oldest))
+        print("\nСамый молодой человек:\n")
+        print(format_profile(youngest))
+
+    except (FileNotFoundError, IOError, ValueError) as e:
+        print(f"Ошибка: {e}")
 
 
 if __name__ == "__main__":
