71ad6279c957cfba31e01a5e229fb06ef5ab311c
123321zZzZ <12345612344321zz12@gmail.com>
2025-11-12 21:29:05 +0400

    Create lab1.py
    feat: add project skeleton

---
+++ lab_1/lab1.py
@@ -0,0 +1,12 @@
+"""
+Var_12 Определите, какой код оператора чаще всего 
+встречается в телефонных номерах. Выведите
+на экран код и количество его повторений.
+"""
+
+import argparse
+import re
+from typing import List, Dict, Tuple, Optional
+def main() -> None:
+if __name__ == "__main__":
+    main()
