36c08cc3912d734d03501578027c41340dfe2b15
kortunova <kortunova2006@mail.ru>
2025-11-15 15:31:29 +0400

    Сделала лабу 5

---
+++ lab5/ui_main_window.py
@@ -0,0 +1,99 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1027, 683)
+        MainWindow.setStyleSheet("\n"
+"QMainWindow {\n"
+"    background-color: #f0f0f0;\n"
+"}\n"
+"\n"
+"QPushButton {\n"
+"    background-color: white;\n"
+"    color: black;\n"
+"    border: 1px solid #cccccc;\n"
+"    border-radius: 5px;\n"
+"    padding: 8px 16px;\n"
+"    font-weight: bold;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: #e6e6e6;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: #d4d4d4;\n"
+"}\n"
+"\n"
+"QLabel {\n"
+"    background-color: white;\n"
+"    border: 1px solid #cccccc;\n"
+"    border-radius: 5px;\n"
+"}\n"
+"   ")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        
+        # Основной горизонтальный layout
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(20, 30, 981, 591))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        
+        # Кнопка Back
+        self.pushButton_2 = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_2.setMinimumSize(QtCore.QSize(100, 40))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        
+        # Область для изображения - убираем автоматическое масштабирование
+        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label.setMinimumSize(QtCore.QSize(600, 500))  
+        self.label.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setText("")
+        
+        pixmap = QtGui.QPixmap("../../../Users/Настя/Desktop/bears/additional/000012.jpg")
+        if not pixmap.isNull():
+            scaled_pixmap = pixmap.scaled(self.label.size(), QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
+            self.label.setPixmap(scaled_pixmap)
+        
+        self.label.setScaledContents(False)  
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        
+        # Кнопка Next
+        self.pushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton.setMinimumSize(QtCore.QSize(100, 40))
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1027, 22))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionSelectSource = QtWidgets.QAction(MainWindow)
+        self.actionSelectSource.setObjectName("actionSelectSource")
+        self.menuFile.addAction(self.actionSelectSource)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Просмотр датасета медведей"))
+        self.pushButton_2.setText(_translate("MainWindow", "Back"))
+        self.pushButton.setText(_translate("MainWindow", "Next"))
+        self.menuFile.setTitle(_translate("MainWindow", "Файл"))
+        self.actionSelectSource.setText(_translate("MainWindow", "Выбрать источник"))
