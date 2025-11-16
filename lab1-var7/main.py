f7d1663b518e61453fe0e1873afb67638ed675c8
faitl <klfvera@gmail.com>
2025-11-16 16:54:48 +0400

    исправила ошибки

--- lab1-var7/main.py
+++ lab1-var7/main.py
@@ -3,26 +3,27 @@ import re
 
 
 def is_valid_phone(phone: str) -> bool:
-    '''
+    """
     проверка валидности номера телефона с кодом 927
-    '''
+    """
     pattern = r'^(?:\+7|8)\s*\(?927\)?[\s-]?\d{3}[\s-]?\d{2}[\s-]?\d{2}$'
     match = re.search(pattern, phone)
     return bool(match)
 
 
 def extract_records_with_927(text: str) -> list[str]:
-
-    #разделение анкет и выбор тех, где телефон имеет код 927
-
+    """
+    разделение анкет и выбор тех, где телефон имеет код 927
+    """
     records = re.split(r'\n\d+\)\n|\n\s*\n', text.strip())
+
     valid_records = []
 
     for record in records:
         for line in record.split('\n'):
-            if re.match(r'\s*Номер телефона или email:', line):
-                contact = line.split(':', 1)[1].strip()
-
+            m = re.match(r'\s*Номер телефона или email:\s*(.+)', line)
+            if m:
+                contact = m.group(1).strip()
                 contact_cleaned = re.sub(r'\s+', ' ', contact)
                 if is_valid_phone(contact_cleaned):
                     valid_records.append(record.strip())
@@ -32,17 +33,17 @@ def extract_records_with_927(text: str) -> list[str]:
 
 
 def read_file(file_name: str) -> str:
-
-    #чтение данных из файла
-
+    """
+    чтение данных из файла
+    """
     with open(file_name, "r", encoding="utf-8") as file:
         return file.read()
 
 
 def write_file(records: list[str]) -> None:
-
-    #запись найденных анкет в файл result.txt
-
+    """
+    запись найденных анкет в файл result.txt
+    """
     with open("result.txt", "w", encoding="utf-8") as file:
         for record in records:
             file.write(record + "\n\n")
