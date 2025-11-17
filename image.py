313a1783ee612017b7fa651fa6696f3c9ebdd1c7
Anna <anutka893706@gmail.com>
2025-11-17 12:36:29 +0400

    внесла корректировки в 2 лабу

--- image.py
+++ image.py
@@ -8,7 +8,9 @@ from icrawler.builtin import GoogleImageCrawler
 
 class ImageIterator:
     """Class for iteration by images"""
-    def __init__(self, source):
+
+    def __init__(self, source: str):
+        """Constructor that accept string path to csv or directory"""
         self.paths = []
         self.counter = 0
 
@@ -41,46 +43,55 @@ class ImageIterator:
 
 def parse_date_range(date_range_str: str) -> list[date]:
     """Parse string to list of dates"""
-    date_range = list(map(int, date_range_str.split('-')))
+    date_range = list(map(int, date_range_str.split("-")))
     if len(date_range) != 6:
         raise ValueError(f"Invalid date format. Usage: YYYY-MM-DD-YYYY-MM-DD")
-    return [date(date_range[0], date_range[1], date_range[2]), date(date_range[3], date_range[4], date_range[5])]
+    return [
+        date(date_range[0], date_range[1], date_range[2]),
+        date(date_range[3], date_range[4], date_range[5]),
+    ]
 
 
-def download_horse_images(start: date, end: date, directory: str, count: int) -> list[str]:
+def download_horse_images(
+    start: date,
+    end: date,
+    directory: str,
+    count: int,
+    keywords: list[str] = [
+        "хобби хорсер",
+        "hobby horser",
+        "hobbyhorser",
+        "horse",
+        "pony",
+        "horsephoto",
+    ],
+) -> list[str]:
     """Download images with horses in specified dates range"""
-
     os.makedirs(directory, exist_ok=True)
-    keywords = ["хобби хорсер", "hobby horser", "hobbyhorser", "horse", "pony", "horsephoto"]
     downloaded_paths = set()
     if not (os.listdir(directory) is None):
         for file_path in os.listdir(directory):
-            if file_path.endswith(('.jpg', '.jpeg', '.png')):
+            if file_path.endswith((".jpg", ".jpeg", ".png")):
                 downloaded_paths.add(file_path)
     i = 0
     while len(downloaded_paths) < count and i < len(keywords):
         try:
-            filters = dict(
-                type='photo',
-                date=(start, end)
-            )
+            filters = dict(type="photo", date=(start, end))
             crawler = GoogleImageCrawler(
-                storage={'root_dir': directory},
-                downloader_threads=4,
-                log_level=50
+                storage={"root_dir": directory}, downloader_threads=4, log_level=50
             )
 
             crawler.crawl(
                 keyword=keywords[i],
                 filters=filters,
                 max_num=count - len(downloaded_paths),
-                file_idx_offset=len(downloaded_paths)
+                file_idx_offset=len(downloaded_paths),
             )
 
             time.sleep(2)
             if not (os.listdir(directory) is None):
                 for file_path in os.listdir(directory):
-                    if file_path.endswith(('.jpg', '.jpeg', '.png')):
+                    if file_path.endswith((".jpg", ".jpeg", ".png")):
                         downloaded_paths.add(file_path)
 
             actual_downloaded = len(downloaded_paths)
@@ -94,14 +105,15 @@ def download_horse_images(start: date, end: date, directory: str, count: int) ->
     return sorted(downloaded_paths)
 
 
-def create_annotation_csv(image_paths: list[str], annotation_file: str, directory: str) -> None:
+def create_annotation_csv(
+    image_paths: list[str], annotation_file: str, directory: str
+) -> None:
     """Generator csv annotation"""
-    with open(annotation_file, 'w', newline='', encoding='utf-8') as csvfile:
+    with open(annotation_file, "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['Absolute Path', 'Relative Path'])
+        writer.writerow(["Absolute Path", "Relative Path"])
 
         for path in image_paths:
             abs_path = os.path.abspath(os.path.join(directory, path))
             rel_path = os.path.join(directory, path)
             writer.writerow([abs_path, rel_path])
-
--- main.py
+++ main.py
@@ -6,38 +6,52 @@ import image
 def get_args() -> list[str]:
     """Parse cmd arguments"""
     parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--directory", help="Directory where image will be saved")
+    parser.add_argument("-f", "--annotation_file", help="Filename annotation csv file")
     parser.add_argument(
-        '-o', '--directory', help='Directory where image will be saved'
+        "-d", "--date_range", help="Date range (format YYYY-MM-DD-YYYY-MM-DD)"
     )
+    parser.add_argument("-c", "--count", help="How many images will be download")
     parser.add_argument(
-        '-f', '--annotation_file', help='Filename annotation csv file'
-    )
-    parser.add_argument(
-        '-d', '--date_range', help='Date range (format YYYY-MM-DD-YYYY-MM-DD)'
-    )
-    parser.add_argument(
-        '-c', '--count', help='How many images will be download'
+        "-l", "--list", nargs="*", help="Set keywords for image download"
     )
     args = parser.parse_args()
-    if args.directory is None or args.annotation_file is None or args.date_range is None or args.count is None:
+    if (
+        args.directory is None
+        or args.annotation_file is None
+        or args.date_range is None
+        or args.count is None
+    ):
         return None
-    return [args.directory, args.annotation_file, args.date_range, args.count]
+    return [
+        args.directory,
+        args.annotation_file,
+        args.date_range,
+        args.count,
+        args.list,
+    ]
 
 
 def main() -> None:
     """Main function"""
     try:
-        directory, annotation_file, date_range, count = get_args()
-        # python main.py -o res -f data.csv -d 2001-01-01-2023-05-05 -c 52
+        directory, annotation_file, date_range, count, keywords = get_args()
+        # python main.py -o res -f data.csv -d 2001-01-01-2023-05-05 -c 52 -l [keywords]
         start, end = image.parse_date_range(date_range)
     except TypeError:
-        print("Usage: python main.py -o download_dir -f data.csv -d YYYY-MM-DD-YYYY-MM-DD -c count of images")
+        print(
+            "Usage: python main.py -o download_dir -f data.csv -d YYYY-MM-DD-YYYY-MM-DD -c count of images (-l [keywords] optional)"
+        )
         return
     except Exception as e:
         print(f"Something went wrong: {e}")
         return
-
-    image_paths = image.download_horse_images(start, end, directory, int(count))
+    if keywords is None:
+        image_paths = image.download_horse_images(start, end, directory, int(count))
+    else:
+        image_paths = image.download_horse_images(
+            start, end, directory, int(count), keywords
+        )
 
     print(f"\nDownloaded {len(image_paths)} images")
 
@@ -49,5 +63,5 @@ def main() -> None:
         print(path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
