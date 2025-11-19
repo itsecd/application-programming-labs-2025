9b6e496ba4fea6f9b3ff38df17da6d81909f5532
Povelitel-kiber-ezikov <mr.timur1705@gmail.com>
2025-11-19 18:01:26 +0400

    добавил функцию которая добавляет новкю коллонку со всеми средними амплитудами

--- lab4_var24/lab4.py
+++ lab4_var24/lab4.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import soundfile as sf
+import matplotlib.pyplot as plt
 
 def create_df(path_csv):
     """
@@ -18,3 +19,12 @@ def get_amplitude(path_audio):
     return mean_amp
 
 
+
+def add_amplitude(df):
+    amplitudes = []
+    for idx, row in df.iterrows():
+        amplitude = get_amplitude(row['absolute_path'])
+        amplitudes.append(amplitude)
+    df['amplitude'] = amplitudes
+    return df    
+        
