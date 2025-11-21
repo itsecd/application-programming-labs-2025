7be611789a71b6363a21e5513b373520df75e976
Danil Morozov <danjirelo@yandex.ru>
2025-11-21 14:52:34 +0300

    add iterator module

---
+++ iterator.py
@@ -0,0 +1,39 @@
+import csv
+from typing import Iterator, List
+
+
+class Iterator:
+    def __init__(self, csv_path: str) -> None:
+        self.paths = self.paths_get()
+        self.index = 0
+        self.csv_path = csv_path
+
+
+
+    def __next__(self) -> str:
+        if self.index < len(self.paths):
+
+            res = self.paths[self.index]
+            self.index += 1
+            return res
+        raise StopIteration
+
+
+    def __iter__(self) -> Iterator[str]:
+        return self
+
+
+    def paths_get(self) -> List[str]:
+        """get path"""
+
+
+        paths = []
+        
+        with open(self.csv_path, newline='', encoding='utf-8-sig') as file:
+            r = csv.reader(file)
+
+            next(r)
+            for row in reader:
+                paths.append(row[0])
+
+        return paths
