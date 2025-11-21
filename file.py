56d4ce33485afb90b76e4a09a7b801bb1cb3b2ce
Danil Morozov <danjirelo@yandex.ru>
2025-11-21 14:43:18 +0300

    add wtiter to file module

---
+++ file.py
@@ -0,0 +1,34 @@
+import csv
+import os
+
+
+
+
+def create_annotation_csv(images_path: str, csv_path: str) -> None:
+    """
+    create .csv file
+    """
+
+
+    images = []
+
+
+    for f in os.listdir(images_path):
+        name = f.lower()
+        if name.endswith(".jpeg") or name.endswith(".png"):
+            images.append(f)
+
+
+    with open(csv_path, mode='w', newline='', encoding='utf-8-sig') as file:
+
+        writer = csv.writer(file)
+
+        writer.writerow(["absolute", "relative"])
+
+
+
+        for filename in images:
+            abs = os.path.abspath(os.path.join(images_path, filename))
+
+            rel = os.path.relpath(abs_path, start=os.getcwd())
+            writer.writerow([abs_path, rel_path])
