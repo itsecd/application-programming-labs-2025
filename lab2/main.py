6ef1ab9244182c460804df3bbbbeebfc09872fe8
clayw <clayworks11337@gmail.com>
2025-11-21 12:56:01 +0400

    Создал функция для парсинга значений с консоли и скачивания изображений

--- lab2/main.py
+++ lab2/main.py
@@ -1,21 +1,33 @@
-import csv
-from icrawler.builtin import GoogleImageCrawler
-import requests
+import argparse
+import os
+from icrawler.builtin import BingImageCrawler
 
-def get_keyword() -> str:
+def parse_args() -> argparse.Namespace:
 	"""
-	Получение от пользователя ключевых слов
-	"""
-	keyword = input("Введите ключевое слово: ")
-	return keyword
+    Парсинг параметров с консоли
+    """
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--output", "-o", type=str, required=True, help="Путь к папке для сохранения изображений")
+	parser.add_argument("--keywords", "-k",nargs='+', type=str, required=True, help="Ключевое слово для скачивания изображений")
+	return parser.parse_args()
+
+def download_images(output_dir=str, keywords=set) -> None:
+	for kword in keywords:
+		range_dir = os.path.join(output_dir,kword)
+		os.makedirs(range_dir, exist_ok=True)
+		crawler = BingImageCrawler(storage={'root_dir':range_dir})
+		crawler.crawl(keyword=kword)
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
 
 def main() -> None:
-	try:
-		keyword1 = "dog" # заменить потом
-		keyword2 = "cat" # заменить потом
-		print(keyword1, keyword2)
-	except Exception as exc:
-		print(f"Возникла ошибка: {exc}")
+	""""""
+	args = parse_args()
+	download_images(args.output, args.keywords)
+    
+    
+
+
 
 if __name__ == "__main__":
 	main()
