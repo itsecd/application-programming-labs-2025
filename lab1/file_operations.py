7721b89f0fde1e0137e17fb686f7dcc7eef3a90c
Gwyn1300 <maximus010683vfrc@gmail.com>
2025-11-21 17:25:33 +0400

    Выполнена 1 лабораторная работа

---
+++ lab1/file_operations.py
@@ -0,0 +1,32 @@
+import os
+
+def read_file(path: str) -> str:
+    ''' Читает файл и возвращает строку.'''
+    try:
+        with open(path, "r", encoding='utf-8') as file:
+            lines = file.readlines()
+            return [line.strip() for line in lines]
+    except FileNotFoundError:
+        print(f"Файл {path} не найден")
+        return None
+    except Exception as e:
+        print(f"Ошибка чтения файла: {e}")
+        return None
+
+
+def write_processed_file(original_path: str, processed_lines: str) -> bool:
+    ''' Записывает обработанные данные в новый файл.''' 
+    try:
+        directory = os.path.dirname(original_path)
+        new_file_path = os.path.join(directory, 'data_processed.txt')
+        
+        with open(new_file_path, 'w', encoding='utf-8') as new_file:
+            for line in processed_lines:
+                new_file.write(line + '\n')
+        
+        print(f"Файл успешно создан: {new_file_path}")
+        return True
+        
+    except Exception as e:
+        print(f"Ошибка при создании файла: {e}")
+        return False
