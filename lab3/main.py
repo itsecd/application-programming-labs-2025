ed9ddc79aaf414919168c07fdf48ac520e52667d
clayw <clayworks11337@gmail.com>
2025-11-22 15:24:50 +0400

    Добавлена функция для сохранения изображений с рамками

--- lab3/main.py
+++ lab3/main.py
@@ -1,6 +1,9 @@
-from download_images import parse_args, download_images, create_annotation, FileIterator
 import cv2
 import numpy
+import os
+from download_images import parse_args, download_images, create_annotation, FileIterator
+import matplotlib.pyplot as plt 
+
 
 def paint_frame(img: numpy.ndarray, height: int , width: int, thickness: int, color: tuple = (123,0,50)) -> None:
     """Рисование рамки на изображении"""
@@ -13,14 +16,23 @@ def paint_frame(img: numpy.ndarray, height: int , width: int, thickness: int, co
 
     cv2.rectangle(img, (x1,y1), (x2,y2), color, thickness)
 
+def save_new_images(arr_imgs: dict, result_dir: str) -> None:
+    """Сохранение изображений с рамками по ключевым словам в заданную директорию"""
+    for path, framed_img in arr_imgs.items():
+        category_dir = os.path.join(result_dir, os.path.basename(os.path.dirname(path)))
+        os.makedirs(category_dir, exist_ok=True)
+
+        frame_img_path = os.path.join(category_dir, f"frame_{os.path.basename(path)}")
+        cv2.imwrite(frame_img_path, framed_img)
+        print(f"Сохранено: {frame_img_path}")
 
 def main() -> None:
     args = parse_args()
 
     try:
-        # download_images(args.output, args.keywords)
+        download_images(args.output, args.keywords)
         create_annotation(args.output, args.annotation)
-        image_array = []
+        arr_framed_imgs = {}
 
         files_iterator = FileIterator(args.output)
         for path in files_iterator:
@@ -30,10 +42,16 @@ def main() -> None:
             image_shapes = f"{height} height x {width} width ({channels} channels)"
 
             paint_frame(image, height, width, int(width*0.02))
-            image_array.append(image)
-            cv2.imshow(image_shapes, image)  # отображение
-            cv2.waitKey(0)  
+            arr_framed_imgs[path] = image
 
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # OpenCV использует BGR
+            plt.figure(image_shapes)
+            plt.imshow(image_rgb)
+
+            plt.axis('off')  # убрать оси
+            plt.show()
+            
+        save_new_images(arr_framed_imgs, args.result)
            
     except Exception as e:
         print(f"Произошла ошибка: {e}")
