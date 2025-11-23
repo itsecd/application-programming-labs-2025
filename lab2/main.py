495da03cc78fc78871a8890f247a78f35e91c332
meow <rinegeto@gmail.com>
2025-11-23 14:49:53 +0400

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,70 +1,48 @@
 import argparse
-from typing import Iterator
-
-
-class AnnotationIterator:
-
-
-    def __init__(self, source: str):
-        """
-        задание источника 
-        """
-        self.source = source
-        self.index = 0
-        self.data = []
-
-    def __iter__(self) -> Iterator[str]:
-        """ 
-        возврат итератора 
-        """
-        return self
-
-    def __next__(self) -> str:
-        """ 
-        получение следующего элемента 
-        """
-        if self.index >= len(self.data):
-            raise StopIteration
-
-        value = self.data[self.index]
-        self.index += 1
-        return value
-
-
-def download_images(ranges: list[str], directory: str) -> None:
-    """ 
-    установка изображений 
-    """
-    pass
-
-
-def make_grayscale(directory: str) -> None:
-    """ 
-    перевод в полутон 
-    """
-    pass
-
-
-def write_csv(annotation: str, directory: str) -> None:
-    """ 
-    создание аннотации 
-    """
-    pass
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("keyword", type=str)
-    parser.add_argument("annotation", type=str)
-    parser.add_argument("ranges", nargs="+")
-    return parser.parse_args()
+import os
+import functions
 
 
 def main():
-    args = parse_args()
+    try:
+        parser = argparse.ArgumentParser(
+            description='Скачивание изображений по ключевому слову dog'
+        )
 
-    print("Аргументы получены:")
-    print(args)
+        parser.add_argument("directory", type=str, help="путь к папке")
+        parser.add_argument("annotation", type=str, help="путь к csv-файлу")
+        parser.add_argument("ranges", nargs='+', type=functions.parse_size_range,
+                            help="пример: 100x100-300x300 400x400-800x800")
+
+        args = parser.parse_args()
+
+        directory = args.directory
+        annotation = args.annotation
+
+        functions.annotation_check(annotation)
+
+        os.makedirs(directory, exist_ok=True)
+
+        functions.clear_dir(directory)
+
+        functions.download_images(args.ranges, directory)
+
+        functions.make_grayscale(directory)
+
+        functions.write_csv(annotation, directory)
+
+
+        images = functions.AnnotationIterator(annotation)
+
+        for img in images:
+            print(img)
+
+    except IndexError:
+        print('ошибка в диапазоне размеров')
+    except ValueError as e:
+        print(e)
+    except NotADirectoryError:
+        print('ошибка пути к папке')
 
 
 if __name__ == "__main__":
