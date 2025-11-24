a08aabe943878beb8fc39584b0c4bff3b902eed5
marulias <markova.yula140@gmail.com>
2025-11-24 23:04:39 +0400

    Update functions.py
    исправила замечания

--- lab4/functions.py
+++ lab4/functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from PIL import Image
 from typing import List
+import matplotlib.pyplot as plt
 
 def read_annotation_file(annotation_file: str) -> pd.DataFrame:
     
@@ -14,13 +15,12 @@ def calculate_aspect_ratios(df: pd.DataFrame) -> pd.DataFrame:
     
     try:
         aspect_ratios = []
-        for path in df['Абсолютный путь']:
+        for path in df['absolute_path']:
             try:
                 with Image.open(path) as img:
                     width, height = img.size
                     aspect_ratios.append(round(width / height, 2))
             except Exception as e:
-                print(f"Ошибка при обработке изображения {path}: {e}")
                 aspect_ratios.append(1.33)  # значение по умолчанию
 
         df['aspect_ratio'] = aspect_ratios
@@ -49,5 +49,14 @@ def filter_by_range(df: pd.DataFrame, range_filter: str) -> pd.DataFrame:
     try:
         return df[df['range'] == range_filter]
     except Exception as e:
-
         raise Exception(f"Ошибка при фильтрации: {e}")
+
+def create_histogram(df: pd.DataFrame, filename: str = 'histogram.png') -> None:
+    
+    plt.figure(figsize=(8, 5))
+    df['range'].value_counts().sort_index().plot(kind='bar')
+    plt.title('Распределение по соотношению сторон')
+    plt.xlabel('Диапазон соотношения')
+    plt.ylabel('Количество изображений')
+    plt.savefig(filename)
+    plt.show()
