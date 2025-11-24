9f0a13ecabcca6fcd0b97532971417b68a8703b3
lamtiaf <lamtiaf@mail.ru>
2025-11-24 15:23:43 +0400

    lab3: Add validate_arguments

--- lab3_var7/main.py
+++ lab3_var7/main.py
@@ -17,9 +17,27 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def validate_arguments(input_path: str, threshold: int) -> None:
+    """
+    Проверка корректности аргументов командной строки.
+    """
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f"Файл не найден: {input_path}")
+    
+    if not 0 <= threshold <= 255:
+        raise ValueError("Порог должен быть в диапазоне от 0 до 255")
+    
+
 def main() -> None:
 
     args = parse_arguments()
+    
+    try:
+        validate_arguments(args.input, args.threshold)
+        print(f"Аргументы корректны: input={args.input}, output={args.output}, threshold={args.threshold}")
+        
+    except Exception as e:
+        print(f"Ошибка: {e}")
 
 
 if __name__ == "__main__":
