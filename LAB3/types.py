7364ed1ac4da3ad009e60fea1cd0c4374ce99b3c
KoshechkinDmitry <koshechkin18@gmail.com>
2025-11-24 16:56:17 +0400

    удалил ненужные файлы

--- LAB3/types.py
+++
@@ -1,8 +0,0 @@
-from typing import Tuple, Union
-import numpy as np
-import numpy.typing as npt
-
-# Типы для работы с изображениями
-ImageArray = npt.NDArray[np.uint8]
-ImageShape = Tuple[int, int, int]
-ImagePath = Union[str, bytes]
