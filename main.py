0981e17cd0d3f8d552f6d8834f7e3d2fa003d425
Виноградов Иван 6212 <em@exa.com>
2025-11-24 18:24:43 +0400

    Создание основного CLI интерфейса

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 import argparse
-from pathlib import Path
 
 from core import download_images, create_annotation_csv
 from paths_iterator import ImageList
 
 
 def main():
+    """Основная функция CLI интерфейса."""
     parser = argparse.ArgumentParser(description="Загрузка изображений")
     parser.add_argument("--keyword", type=str, default="cat")
     parser.add_argument("--num_images", type=int, default=50)
-    parser.add_argument("--min_size", type=int, nargs=2, default=[200, 200])
-    parser.add_argument("--max_size", type[int], nargs=2, default=[5000, 5000])
-    parser.add_argument("--save_dir", type=str, default="./images")
-    parser.add_argument("--csv_path", type=str, default="./annotation.csv")
 
-    a = parser.parse_args()
+    args = parser.parse_args()
 
-    if not (50 <= a.num_images <= 1000):
+    if not (50 <= args.num_images <= 1000):
         print("Количество изображений должно быть от 50 до 1000.")
         return
 
     download_images(
-        a.keyword,
-        a.num_images,
-        tuple(a.min_size),
-        tuple(a.max_size),
-        a.save_dir
+        args.keyword,
+        args.num_images,
+        (200, 200),
+        (5000, 5000),
+        "./images"
     )
 
-    count = create_annotation_csv("try_image", a.csv_path)
-    print(f"CSV создан: {a.csv_path}, файлов: {count}")
+    print("Загрузка завершена.")
 
 
 if __name__ == "__main__":
