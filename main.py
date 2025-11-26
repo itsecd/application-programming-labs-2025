d6c2f61b28c3533256d8ce3d10bb8885e7a1313e
FxntmCode <alannn.oka@gmail.com>
2025-11-26 21:56:59 +0300

    сделал по PEP8

--- main.py
+++ main.py
@@ -3,64 +3,55 @@ import soundfile as sf
 import argparse
 import matplotlib.pyplot as plt
 
+
 def parser() -> argparse.Namespace:
     """
     Парсит аргументы командной строки
     """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        '-i', '--input',
-        type = str,
-        help = 'Путь до аудио файла'
-    )
+    parser.add_argument("-i", "--input", type=str, help="Путь до аудио файла")
 
     parser.add_argument(
-        '-o', '--output',
-        type = str,
-        default = 'output.mp3',
-        help = 'Название обрезанного файла'
-    )
-    
-    parser.add_argument(
-        '-s', '--start',
-        type = int,
-        help = 'Начало файла (В секундах)'
+        "-o",
+        "--output",
+        type=str,
+        default="output.mp3",
+        help="Название обрезанного файла",
     )
 
-    parser.add_argument(
-        '-e', '--end',
-        type = int,
-        help = 'Конец файла (В секундах)'
-    )
+    parser.add_argument("-s", "--start", type=int, help="Начало файла (В секундах)")
+
+    parser.add_argument("-e", "--end", type=int, help="Конец файла (В секундах)")
 
     return parser.parse_args()
 
 
-def trim_audio(path : str, start : int, end : int, output : str) -> None:
+def trim_audio(path: str, start: int, end: int, output: str) -> None:
     """
     Осуществляет обрезку аудио
     """
     try:
         data, samplerate = sf.read(path)
-        num_samples, minutes, sec = file_info(path)    
+        num_samples, minutes, sec = file_info(path)
 
         if end > ((minutes * 60) + sec):
             raise ValueError("Нельзя отрезать больше положенного ;)")
         if start < 0:
             raise ValueError("Нельзя добавить больше положенного ;)")
-        
+
         start_sample = samplerate * start
         end_sample = samplerate * end
 
         trim = data[start_sample:end_sample]
         sf.write(output, trim, samplerate)
     except FileNotFoundError as e:
-        print(f"Файл не найден: {e}") 
+        print(f"Файл не найден: {e}")
 
-def file_info(path : str) -> tuple[int, int, int]:
+
+def file_info(path: str) -> tuple[int, int, int]:
     """
-    Возвращает информацию о файле 
+    Возвращает информацию о файле
     """
     try:
         data, samplerate = sf.read(path)
@@ -69,10 +60,11 @@ def file_info(path : str) -> tuple[int, int, int]:
 
         minutes = int(duration // 60)
         sec = int(duration % 60)
-        
+
         return num_samples, minutes, sec
     except FileNotFoundError as e:
         print(f"Файл не найден: {e}")
+
 
 def show_wave(path: str, title: str) -> None:
     data, sr = sf.read(path)
@@ -83,8 +75,8 @@ def show_wave(path: str, title: str) -> None:
     plt.figure(figsize=(12, 4))
     plt.plot(times, data)
     plt.title(title)
-    plt.ylabel('Амплитуда')
-    plt.xlabel('Время в секундах')
+    plt.ylabel("Амплитуда")
+    plt.xlabel("Время в секундах")
     plt.tight_layout()
     plt.show()
 
@@ -94,11 +86,11 @@ def main() -> None:
         args = parser()
         num_samples, minutes, sec = file_info(args.input)
         print(f"Размер аудиофайла: {num_samples} сэмплов")
-        trim_audio(args.input , args.start, args.end, args.output)
-        show_wave(args.output, 'Обрезанный файл')
+        trim_audio(args.input, args.start, args.end, args.output)
+        show_wave(args.output, "Обрезанный файл")
     except Exception as e:
         print(f"Произошла ошибка: {e}")
 
 
 if __name__ == "__main__":
-    main() 
+    main()
