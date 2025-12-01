79c5f0020f2a0289ea31f8be67fc0a75cb770490
Руслан <ruslanformail@gmail.com>
2025-12-02 01:43:22 +0400

    отформатировал код

---
+++ lab1/lab1.py
@@ -0,0 +1,65 @@
+import re
+import argparse
+
+
+def get_args():
+    """Получение аргументов из командной строки"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("in_file", type=str, help="input file")
+    parser.add_argument("out_file", type=str, help="output file")
+    return parser.parse_args()
+
+
+def read_file(filename):
+    """Чтение содержимого файла"""
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            return f.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Файл {filename} не найден")
+    except Exception as e:
+        raise Exception(f"Ошибка при чтении файла: {e}")
+
+
+def find_moscow(data):
+    """Поиск анкет из Москвы"""
+    pattern = r"Город:\s*(г\.?\s*)?[Мм]осква\b"
+    result = []
+    forms = data.split("\n\n")
+    
+    for form in forms:
+        if re.search(pattern, form):
+            result.append(form)
+    
+    return result
+
+
+def save_data(data_list, out_path):
+    """Сохранение данных в новый файл"""
+    try:
+        with open(out_path, "w", encoding="utf-8") as f:
+            for item in data_list:
+                f.write(item + "\n\n")
+        print(f"Анкеты сохранены в файл: {out_path}")
+    except Exception as e:
+        raise Exception(f"Ошибка при сохранении файла: {e}")
+
+
+def main():
+    try:
+        args = get_args()
+        text = read_file(args.in_file)
+        moscow_list = find_moscow(text)
+        print(f"Найдено анкет: {len(moscow_list)}")
+
+        if moscow_list:
+            save_data(moscow_list, args.out_file)
+        else:
+            print("Анкеты жителей Москвы не найдены, файл не создан.")
+            
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+
+if __name__ == '__main__':
+    main()
