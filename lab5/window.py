20c7a7e555c5dc2f9c379b1d35963d0d66f7bbf9
meow <rinegeto@gmail.com>
2025-12-03 10:33:55 +0400

    создала скелет работы

---
+++ lab5/window.py
@@ -0,0 +1,34 @@
+import sys
+from typing import Optional
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from iterator import AnnotationIterator
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    """
+    Главное окно приложения для просмотра датасета
+    """
+    
+    def __init__(self) -> None:
+        pass
+
+    def select_folder(self) -> None:
+        pass
+
+    def show_image(self) -> None:
+        pass
+
+    def show_next(self) -> None:
+        pass
+
+    def show_prev(self) -> None:
+        pass
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
