369eb6417cbceb4f379453f73533b92f58e8aa4e
gitKoblas <Jarikpla@yandex.ru>
2025-12-04 10:22:02 +0400

    Добавил кнопку для выбора файла аннотации

--- lab5/main_window.py
+++ lab5/main_window.py
@@ -1,4 +1,3 @@
-import argparse
 import sys
 
 from PySide6.QtWidgets import (
@@ -8,20 +7,13 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QLabel,
     QApplication,
+    QFileDialog
 )
 from PySide6.QtGui import QPixmap
 from PySide6.QtCore import Qt
 
 
 from Lab2 import Path_Iterator
-
-
-def parsing() -> str:
-    """передача аргументов через командную строку"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("annotation_path", type=str)
-    args = parser.parse_args()
-    return args.annotation_path
 
 
 class Gallery(QMainWindow):
@@ -38,19 +30,30 @@ class Gallery(QMainWindow):
         self.img_label.setAlignment(Qt.AlignCenter)
         self.button_next = QPushButton("Следующие изображение")
         self.button_next.setEnabled(False)
+        self.button_load_annotation = QPushButton("Добавьте файл аннотации")
         self.button_next.clicked.connect(self.next_img)
+        self.button_load_annotation.clicked.connect(self.load_annotation)
+        layout.addWidget(self.button_load_annotation)
         layout.addWidget(self.img_label)
         layout.addWidget(self.button_next)
         self.iterator = None
         self.current_item = None
 
-    def load_annotation(self, annotation_path: str):
+    def load_annotation(self):
         """получение изображений из файла аннотации"""
-        self.iterator = Path_Iterator(annotation_path)
-        self.iterator.index = 0
-        self.current_item = next(self.iterator)
-        self.show_img(self.current_item)
-        self.button_next.setEnabled(True)
+        try:
+            file_path, _ = QFileDialog.getOpenFileName(
+            self, "Выберите csv файл", "", "CSV Files(*.csv)"
+            )
+            self.iterator = Path_Iterator(file_path)
+            self.iterator.index = 0
+            self.current_item = next(self.iterator)
+            self.show_img(self.current_item)
+            self.button_next.setEnabled(True)
+        except Exception as ex:
+            print(f"Ошибка: {ex}")
+            
+
 
     def show_img(self, file_path:str):
         """Отображение изображений"""
@@ -74,10 +77,8 @@ class Gallery(QMainWindow):
 
 def main():
     try:
-        annotation_path = parsing()
         app = QApplication(sys.argv)
         gallery = Gallery()
-        gallery.load_annotation(annotation_path)
         gallery.show()
         sys.exit(app.exec())
     except Exception as ex:
