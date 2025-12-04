5f319c40de1d41840bbdbcc4f91417652b13f4ed
Elisaveta06 <elizaveta.arbi06@mail.ru>
2025-12-04 13:17:35 +0400

    Внесены изменения.

--- image_processor.py
+++ image_processor.py
@@ -2,15 +2,18 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def resize_image(img: np.ndarray, width, height: int) -> np.ndarray:
     """Изменяет размер изображения на заданный."""
     resized = cv2.resize(img, (width, height))
     return resized
 
+
 def get_image_size(img: np.ndarray) -> tuple[int, int]:
     """Возвращает размер изображения (высота, ширина)."""
     h, w = img.shape[:2]
     return h, w
+
 
 def read_image(path: str) -> np.ndarray:
     """Читает изображение из файла."""
@@ -18,6 +21,7 @@ def read_image(path: str) -> np.ndarray:
     if img is None:
         print(f"Ошибка: не удалось открыть файл {path}")
     return img
+
 
 def show_image(first_image, second_image: np.ndarray) -> None:
     """
@@ -30,14 +34,15 @@ def show_image(first_image, second_image: np.ndarray) -> None:
 
     axs[0].imshow(first_rgb)
     axs[0].set_title(f"Исходное\n{first_image.shape[1]}x{first_image.shape[0]}")
-    axs[0].axis('off')
+    axs[0].axis("off")
 
     axs[1].imshow(second_rgb)
     axs[1].set_title(f"Результат\n{second_image.shape[1]}x{second_image.shape[0]}")
-    axs[1].axis('off')
+    axs[1].axis("off")
 
-    plt.tight_layout() #Макет интерфейса
-    plt.show() # Выводит всё.
+    plt.tight_layout()  # Макет интерфейса
+    plt.show()  # Выводит всё.
+
 
 def save_image(img: np.ndarray, path: str) -> None:
     """Сохраняет изображение в файл."""
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import argparse
 
-from image_processor import get_image_size, resize_image, read_image, save_image, show_image
+from image_processor import (
+    get_image_size,
+    resize_image,
+    read_image,
+    save_image,
+    show_image,
+)
 from path_utils import get_correct_bild_path
+
 
 def parser_t() -> tuple[str, str, str, int]:
     """
@@ -15,6 +22,7 @@ def parser_t() -> tuple[str, str, str, int]:
     args = parser.parse_args()
     return args.source, args.bild, args.height, args.width
 
+
 def main() -> None:
     try:
         source, bild, height, width = parser_t()
@@ -24,7 +32,7 @@ def main() -> None:
         img = read_image(source)
         if img is None:
             return
-        
+
         w, h = get_image_size(img)
         print(f"Исходный размер: {w} x {h}")
 
@@ -32,14 +40,14 @@ def main() -> None:
             raise ValueError("Длина и ширина должны быть натуральными числами")
 
         resized = resize_image(img, width, height)
-        
+
         w_new, h_new = get_image_size(resized)
         print(f"Новый размер: {w_new} x {h_new}")
 
         show_image(img, resized)
 
         save_image(resized, bild)
-        
+
     except Exception as e:
         print(f"Error: {e}")
 
